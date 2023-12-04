c5803fbaec90ffff8cd98bb18c3e19d075675927
pantat88 <132797949+pantat88@users.noreply.github.com>
2023-12-05 03:10:10 +0700

    Update pantat88.py

--- ui/pantat88.py
+++ ui/pantat88.py
@@ -16,31 +16,31 @@ def download(line):
     args = line.split()
 
     if "civitai.com" in args[0]:
-        link, fn = args[0], args[1] if len(args) > 1 else None
+        url, fn = args[0], args[1] if len(args) > 1 else None
         auth = f"-H 'Authorization: Bearer d3bdbbd15377673b43f7ab4b224f2800'"
         outopt = f"-o {fn}" if fn else "-O"
-        fc = f"curl -J -L {auth} {link} {outopt}"
-
-        result = subprocess.run(fc, shell=True, capture_output=True, text=True, cwd=os.getcwd())
-        dfn = fn or os.path.basename(urlparse(link).path)
-
+        fc = f"curl -J -L {auth} {url} {outopt}"
+        dfn = fn or os.path.basename(urlparse(url).path)
+        
         display(HTML(f"<span>Downloading: {dfn}</span>"))
+        result = subprocess.run(fc, shell=True, capture_output=True, text=True, cwd=os.getcwd())
+
         if result.returncode == 0:
             print("done")
         elif result.stderr:
             print(result.stderr)
             
     else:  
-        link, path, fn = args[0], args[1] if len(args) > 1 else None, args[2] if len(args) > 2 else None
-        fn = os.path.basename(urlparse(link).path) if not path or not fn else fn
-        full_path = os.path.join(os.getcwd(), path) if path else None
+        url, path, fn = args[0], args[1] if len(args) > 1 else None, args[2] if len(args) > 2 else None
+        fn = os.path.basename(urlparse(url).path) if not path or not fn else fn
+        fp = os.path.join(os.getcwd(), path) if path else None
         
-        if full_path and os.path.splitext(full_path)[1]:
-            fn = os.path.basename(full_path)
+        if fp and os.path.splitext(fp)[1]:
+            fn = os.path.basename(fp)
             
-        full_path = os.path.join(os.getcwd(), path) if path else None
-        os.makedirs(full_path, exist_ok=True) if full_path else None
-        fc = f"curl -Lo {os.path.join(full_path, fn)} {link}" if full_path else f"curl -Lo {fn} {link}"
+        fp = os.path.join(os.getcwd(), path) if path else None
+        os.makedirs(fp, exist_ok=True) if fp else None
+        fc = f"curl -Lo {os.path.join(fp, fn)} {url}" if fp else f"curl -Lo {fn} {url}"
         
         display(HTML(f"<span>Downloading: {fn}</span>"))
         result = subprocess.run(fc, shell=True, capture_output=True, text=True, cwd=os.getcwd())
