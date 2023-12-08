08c1f8fde06ae4e842fc36a000772c0f87cac030
pantat88 <132797949+pantat88@users.noreply.github.com>
2023-12-08 19:01:39 +0700

    Update pantat88.py

--- ui/pantat88.py
+++ ui/pantat88.py
@@ -1,8 +1,8 @@
 from IPython.core.magic import register_line_magic
-from IPython.display import HTML, display
+from IPython.display import display, HTML
+from urllib.parse import urlparse
 import subprocess
 import os
-from urllib.parse import urlparse
 
 @register_line_magic
 def say(line):
@@ -14,39 +14,34 @@ def say(line):
 @register_line_magic
 def download(line):
     args = line.split()
-
-    if "civitai.com" in args[0]:
-        url, fn = args[0], args[1] if len(args) > 1 else None
-        auth = f"-H 'Authorization: Bearer d3bdbbd15377673b43f7ab4b224f2800'"
-        outopt = f"-o {fn}" if fn else "-O"
-        fc = f"curl -J -L {auth} {url} {outopt}"
-        dfn = fn or os.path.basename(urlparse(url).path)
+    url, auth = args[0], "-H 'Authorization: Bearer d3bdbbd15377673b43f7ab4b224f2800'" if "civitai.com" in args[0] else ""
+    
+    if len(args) == 1:
+        fn = os.path.basename(urlparse(args[0]).path)
+        fc = f"curl -O -J -L {auth} {args[0]} > /dev/null"
+    elif len(args) == 3:
+        path, fn = args[1], args[2]
+        os.makedirs(path, exist_ok=True)
+        fc = f"mkdir -p {path} && cd {path} && curl -J -L {auth} {args[0]} -o {fn} > /dev/null"
+    elif '/' in args[1] or '~/ ' in args[1]:
+        path = args[1]
+        os.makedirs(path, exist_ok=True)
+        fn = os.path.basename(urlparse(args[0]).path)
+        fc = f"mkdir -p {path} && cd {path} && curl -O -J -L {auth} {args[0]} > /dev/null"
+    else:
+        fn = args[1]
+        fc = f"curl -J -L {auth} {args[0]} -o {fn} > /dev/null"
         
-        display(HTML(f"<span>Downloading: {dfn}</span>"))
-        result = subprocess.run(fc, shell=True, capture_output=True, text=True, cwd=os.getcwd())
-
-        if result.returncode == 0:
-            print("done")
-        elif result.stderr:
-            print(result.stderr)
-            
-    else:  
-        url, path, fn = args[0], args[1] if len(args) > 1 else None, args[2] if len(args) > 2 else None
-        fn = os.path.basename(urlparse(url).path) if not path or not fn else fn
-        fp = os.path.join(os.getcwd(), path) if path else None
+    print(f"Downloading: {fn}")
+    
+    try:
+        result = subprocess.run(fc, shell=True, stderr=subprocess.PIPE, text=True, cwd=os.getcwd(), check=True)
+        print("done")
+    except subprocess.CalledProcessError as e:
+        if "curl: (23)" in e.stderr:
+            print("Error: File exists")
+        else:
+            print(e.stderr)
+    except KeyboardInterrupt:
+        print("^ Canceled")
         
-        if fp and os.path.splitext(fp)[1]:
-            fn = os.path.basename(fp)
-            
-        fp = os.path.join(os.getcwd(), path) if path else None
-        os.makedirs(fp, exist_ok=True) if fp else None
-        fc = f"curl -Lo {os.path.join(fp, fn)} {url}" if fp else f"curl -Lo {fn} {url}"
-        
-        display(HTML(f"<span>Downloading: {fn}</span>"))
-        result = subprocess.run(fc, shell=True, capture_output=True, text=True, cwd=os.getcwd())
-        
-        if result and result.returncode == 0:
-            print("done")
-        elif result:
-            print(result.stderr)
-            
