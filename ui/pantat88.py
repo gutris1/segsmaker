24124e6822ef4b8703200e498aac953b0025b02e
pantat88 <132797949+pantat88@users.noreply.github.com>
2023-12-10 14:24:21 +0700

    Update pantat88.py

--- ui/pantat88.py
+++ ui/pantat88.py
@@ -3,13 +3,9 @@ from IPython.display import display, HTML
 from urllib.parse import urlparse
 import subprocess
 import os
-import sys
-import zipfile
-from tqdm import tqdm
 
 @register_line_magic
 def say(line):
-    
     args = line.split()
     msg = " ".join(args[:-1])
     color = args[-1]
@@ -17,7 +13,6 @@ def say(line):
     
 @register_line_magic
 def download(line):
-    
     args = line.split()
     url, auth = args[0], "-H 'Authorization: Bearer d3bdbbd15377673b43f7ab4b224f2800'" if "civitai.com" in args[0] else ""
     
@@ -49,38 +44,4 @@ def download(line):
             print(e.stderr)
     except KeyboardInterrupt:
         print("^ Canceled")
-
-@register_line_magic
-def zipper(line):
-    
-    def zip_folder(input_path, output_path, max_size_mb=20):
         
-        os.makedirs(output_path, exist_ok=True)
-        all_files = []
-        for root, dirs, files in os.walk(input_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                all_files.append(file_path)
-                
-        zip_number = 1
-        current_zip_size = 0
-        current_zip_name = os.path.join(output_path, f"part_{zip_number}.zip")
-        
-        with tqdm(total=len(all_files), desc='zipping : ', bar_format='{desc}{n_fmt}/{total_fmt} {bar} | {percentage:3.0f}% [ {elapsed}<{remaining}, {rate_fmt}{postfix} ]', ncols=100, file=sys.stdout) as pbar:
-            with zipfile.ZipFile(current_zip_name, 'w', compression=zipfile.ZIP_DEFLATED) as current_zip:
-                for file_path in all_files:
-                    file_size = os.path.getsize(file_path)
-                    
-                    if current_zip_size + file_size > max_size_mb * 1024 * 1024:
-                        current_zip.close()
-                        zip_number += 1
-                        current_zip_name = os.path.join(output_path, f"part_{zip_number}.zip")
-                        current_zip = zipfile.ZipFile(current_zip_name, 'w', compression=zipfile.ZIP_DEFLATED)
-                        current_zip_size = 0
-                        
-                    current_zip.write(file_path, os.path.relpath(file_path, input_path))
-                    current_zip_size += file_size
-                    pbar.update(1)
-
-    max_size_mb = 200
-    zip_folder(input_path, output_path, max_size_mb)
