d6be62c8942cedb4909ab05cc79dd9b46c9465b3
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-10 15:30:39 +0700

    Update pantat88.py

--- ui/pantat88.py
+++ ui/pantat88.py
@@ -212,7 +212,7 @@ def delete(line):
 
     subprocess.run(
         f"rm -rf {' '.join([input_path + t for t in targets])} && "
-        f"find {input_path} -type d -name ".ipynb_checkpoints" -exec rm -rf {} +"
+        f"find {input_path} -type d -name '.ipynb_checkpoints' -exec rm -rf {{}} + "
         f"find {input_path}/* -type f ! -name '*.ipynb' -exec rm -f {{}} +"
         f"find {input_path}/* -type d -empty -delete",
         shell=True,
