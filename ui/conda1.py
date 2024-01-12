e105c5766284cc3e8e8161951298ee1188f37e2b
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-13 02:52:21 +0700

    .

--- ui/conda1.py
+++ ui/conda1.py
@@ -5,10 +5,10 @@ def ass(cmd, bacod, color):
     display(HTML(f"<span style='color:{color};'>{bacod}</span>"))
     subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-ass('pip install -q tqdm psutil glib', '', '')
+ass('pip install -q tqdm psutil', '', '')
 from tqdm import tqdm
 import psutil
-ass('conda install -qy conda', 'Installing Conda', 'cyan')
+ass('conda install -qy conda glib', 'Installing Conda', 'cyan')
 ass('conda install -q -y -n base python=3.10.12', 'Installing Python 3.10.12', '#D48900')
 ass('conda clean -y --all', 'Cleaning Conda Environment', '#66ff00')
 ass('pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118', 'Installing Torch', 'magenta')
