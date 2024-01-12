3756f02a16fd58864cdf2e0aef5fe6034428c7f5
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-13 02:17:33 +0700

    .

--- ui/conda1.py
+++ ui/conda1.py
@@ -5,7 +5,7 @@ def ass(command, message, color):
     display(HTML(f"<span style='color:{color};'>・・・ {message} ・・・</span>"))
     result = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-ass('pip install -q tqdm psutil glib')
+ass('pip install -q tqdm psutil glib', '', '')
 ass('conda install -qy conda', 'Installing Conda', 'cyan')
 ass('conda install -q -y -n base python=3.10.12', 'Installing Python 3.10.12', '#D48900')
 ass('conda clean -y --all', 'Cleaning Conda Environment', '#66ff00')
