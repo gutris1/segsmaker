8f5e3905f16a8ec2d20ad4a1b074e299c8745ce4
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-13 01:53:24 +0700

    Update conda1.py

--- ui/conda1.py
+++ ui/conda1.py
@@ -5,10 +5,9 @@ def ass(command, message, color):
     display(HTML(f"<span style='color:{color};'>・・・ {message} ・・・</span>"))
     result = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-ass('conda install -qy conda=23.11.0 glib psutil', 'Installing Conda', 'cyan')
+ass('conda install -qy conda=23.11.0 glib', 'Installing Conda', 'cyan')
 ass('conda install -q -y -n base python=3.10.12', 'Installing Python 3.10.12', '#D48900')
 ass('conda clean -y --all', 'Cleaning Conda Environment', '#66ff00')
 ass('pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118', 'Installing Torch', 'magenta')
-ass('pip install -q xformers==0.0.20 triton==2.0.0', 'Installing xformers', 'orange')
+ass('pip install -q xformers==0.0.20 triton==2.0.0 psutil', 'Installing xformers', 'orange')
 ass('curl -Lo ~/.ipython/profile_default/startup/pantat88.py https://github.com/gutris1/segsmaker/raw/main/ui/asd88.py', 'Gathering Magic', 'red')
-get_ipython().kernel.do_shutdown(True)
