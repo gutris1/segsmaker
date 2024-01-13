d4e946f6dfc6b4a1ae849912e024325fc4c7fff3
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-14 00:51:21 +0700

    Update conda2.py

--- script/conda2.py
+++ script/conda2.py
@@ -5,10 +5,9 @@ def ass(cmd, bacod, color):
     display(HTML(f"<span style='color:{color};'>{bacod}</span>"))
     subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-ass('pip install tqdm psutil pyngrok', '', '')
+ass('pip install tqdm psutil', '', '')
 from tqdm import tqdm
 import psutil
-from pyngrok import ngrok
 
 ass('conda install -y conda glib gxx_linux-64 ffmpeg imageio imageio-ffmpeg av gst-libav', '【 Installing Conda 】', 'cyan')
 ass('conda install -y -n base python=3.10.12', '【 Installing Python 3.10 】', '#D48900')
