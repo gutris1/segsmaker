01228a2b8e6f1996e7decf9b915aedca8faa77df
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-13 16:15:55 +0700

    ,

--- ui/1-5/asd/1-5.py
+++ ui/1-5/asd/1-5.py
@@ -1,6 +1,9 @@
 import subprocess
 
 tempe = [
+    ['rm', '-rf', '~/tmp/*', '~/tmp', '~/asd/models/Stable-diffusion/tmp_models', '~/asd/models/Lora/tmp_Lora', '~/asd/models/ControlNet'],
+    ['mkdir', '-p', '~/asd/models/Lora'],
+    ['mkdir', '-p', '~/asd/models/ESRGAN'],
     ['ln', '-vs', '/tmp', '~/tmp'],
     ['ln', '-vs', '/tmp/models', '~/asd/models/Stable-diffusion/tmp_models'],
     ['ln', '-vs', '/tmp/Lora', '~/asd/models/Lora/tmp_Lora'],
