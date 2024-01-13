9a5cca56d5c5492c9dd873d5591b5ad1ac4c9f28
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-14 00:20:12 +0700

    Update cui2.py

--- ui/cui/asd/cui2.py
+++ ui/cui/asd/cui2.py
@@ -5,7 +5,9 @@ minyak = [
     ['rm', '-rf', '~/tmp/*', '~/tmp', '~/ComfyUI/models/loras', '~/ComfyUI/models/controlnet'],
     ['ln', '-vs', '/tmp', '~/tmp'],
     ['ln', '-vs', '/tmp/Lora', '~/ComfyUI/models/loras'],
-    ['ln', '-vs', '/tmp/ControlNet', '~/ComfyUI/models/controlnet']
+    ['ln', '-vs', '/tmp/ControlNet', '~/ComfyUI/models/controlnet'],
+    ['unzip', '-o', '~/ComfyUI/models/embeddings.zip', '-d', '~/ComfyUI/models/embeddings'],
+    ['rm', '~/ComfyUI/models/embeddings.zip']
 ]
 
 for tepung in minyak:
