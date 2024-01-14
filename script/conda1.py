fe65cb969338d6bc8eb8d7989a4526c8b10ab8d7
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-14 17:50:23 +0700

    Update conda1.py

--- script/conda1.py
+++ script/conda1.py
@@ -5,11 +5,7 @@ def ass(cmd, bacod, color):
     display(HTML(f"<span style='color:{color};'>{bacod}</span>"))
     subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-ass('pip install tqdm psutil', '', '')
-from tqdm import tqdm
-import psutil
-
-ass('conda install -y conda glib', '【 Installing Conda 】', 'cyan')
+ass('conda install -y conda glib psutil', '【 Installing Conda 】', 'cyan')
 ass('conda install -y -n base python=3.10.12', '【 Installing Python 3.10 】', '#D48900')
 ass('conda clean -y --all', '【 Cleaning Conda 】', '#66ff00')
 ass('pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118', '【 Installing Torch 】', 'magenta')
