c853a24160cf75dace7114de0163b53651420012
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-14 22:24:03 +0700

    Update pantat88.py

--- kaggle/script/pantat88.py
+++ kaggle/script/pantat88.py
@@ -31,14 +31,13 @@ def say(line):
 
         if color == '{d}':
             color = default_color
-            
-        elif not color or '{d}':
+        elif not color or color == '{d}':
             if i < len(args) - 1 and re.match(r'^\{[^\{\}]+\}$', args[i + 1].lower()):
                 color = args[i + 1][1:-1]
                 i += 1
-                
             else:
-                msg = line
+                color = default_color
+                msg = args[i]
 
         span_text = f"<span"
         if color:
