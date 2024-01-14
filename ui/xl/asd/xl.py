dcc82707a1dc6e4c043fa33435bb4e606bc897f5
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-14 15:55:01 +0700

    Update xl.py

--- ui/xl/asd/xl.py
+++ ui/xl/asd/xl.py
@@ -2,10 +2,10 @@ import subprocess
 import os
 
 minyak = [
-    ['rm', '-rf', '~/tmp/*', '~/tmp', '~/asd/models/Stable-diffusion', '~/asd/models/Lora', '~/asd/models/ControlNet'],
+    ['rm', '-rf', '~/tmp/*', '~/tmp', '~/asd/models/Stable-diffusion/tmp_models', '~/asd/models/Lora', '~/asd/models/ControlNet'],
     ['mkdir', '-p', '~/asd/models/ESRGAN'],
     ['ln', '-vs', '/tmp', '~/tmp'],
-    ['ln', '-vs', '/tmp/models', '~/asd/models/Stable-diffusion'],
+    ['ln', '-vs', '/tmp/models', '~/asd/models/Stable-diffusion/tmp_models'],
     ['ln', '-vs', '/tmp/Lora', '~/asd/models/Lora'],
     ['ln', '-vs', '/tmp/ControlNet', '~/asd/models/ControlNet']
 ]
