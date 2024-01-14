599330cee27840692f47e407671afb401cdc62c0
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-14 19:59:52 +0700

    Update cui.py

--- kaggle/cui/asd/cui.py
+++ kaggle/cui/asd/cui.py
@@ -5,7 +5,9 @@ minyak = [
     ['rm', '-rf', '/kaggle/temp/*', '/kaggle/temp', '/kaggle/working/ComfyUI/models/checkpoints', '/kaggle/working/ComfyUI/models/loras', '/kaggle/working/ComfyUI/models/controlnet'],
     ['ln', '-vs', '/kaggle/temp/checkpoints', '/kaggle/working/ComfyUI/models/checkpoints'],
     ['ln', '-vs', '/kaggle/temp/loras', '/kaggle/working/ComfyUI/models/loras'],
-    ['ln', '-vs', '/kaggle/temp/controlnet', '/kaggle/working/ComfyUI/models/controlnet']
+    ['ln', '-vs', '/kaggle/temp/controlnet', '/kaggle/working/ComfyUI/models/controlnet'],
+    ['unzip', '-o', '/kaggle/working/ComfyUI/models/embeddings.zip', '-d', '/kaggle/working/ComfyUI/models/embeddings'],
+    ['rm', '/kaggle/working/ComfyUI/models/embeddings.zip']
 ]
 
 for tepung in minyak:
