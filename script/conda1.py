92526a077f5e14aa3d2970fee49898fd3ec23029
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-28 19:52:49 +0700

    Update conda1.py

--- script/conda1.py
+++ script/conda1.py
@@ -5,12 +5,13 @@ def ass(cmd, bacod, color):
     display(HTML(f"<span style='color:{color};'>{bacod}</span>"))
     subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-ass('conda install -y conda glib psutil', '【 Installing Conda 】', 'cyan')
+ass('conda install -y conda glib psutil gperftools', '【 Installing Conda 】', 'cyan')
 ass('conda install -y -n base python=3.10.12', '【 Installing Python 3.10 】', '#D48900')
 ass('conda clean -y --all', '【 Cleaning Conda 】', '#66ff00')
 ass('pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118', '【 Installing Torch 】', 'magenta')
 ass('pip install xformers==0.0.20 triton==2.0.0', '【 Installing xformers 】', 'orange')
 ass('curl -Lo ~/.ipython/profile_default/startup/pantat88.py https://github.com/gutris1/segsmaker/raw/main/script/pantat88.py', '【 Gathering Magic 】', 'red')
+ass('curl -Lo ~/.ipython/profile_default/startup/00-startup.py https://github.com/gutris1/segsmaker/raw/main/script/00-startup.py', '', '')
 display(HTML('<span style="color: cyan;">【 Done 】</span>'))
 
 from IPython import get_ipython
