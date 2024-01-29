85094c37a9a8cb96992d57831d88412fded59633
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-01-30 01:28:42 +0700

    Update zrok.py

--- ui/cui/zrok.py
+++ ui/cui/zrok.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-def hitozuma(token):
+def main(token):
     oppai = subprocess.run(['/home/studio-lab-user/.zrok/bin/zrok', 'enable', token], 
                             check=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
@@ -13,8 +13,8 @@ def hitozuma(token):
     ass = "/home/studio-lab-user/.zrok/bin/zrok share public localhost:8188 --headless 2>&1 | grep 'zrok.io'"
     subprocess.run(ass, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
 
-if __name__ == '__hitozuma__':
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit(1)
 
-    hitozuma(sys.argv[1])
+    main(sys.argv[1])
