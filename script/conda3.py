c80e2f632430341517c793590c156a4fbccb7f1c
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-02-01 19:07:35 +0700

    Update conda3.py

--- script/conda3.py
+++ script/conda3.py
@@ -9,7 +9,6 @@ ass('conda install -y conda glib gxx_linux-64 ffmpeg imageio imageio-ffmpeg av g
 ass('conda install -y -n base python=3.10.12', '【 Installing Python 3.10 】', '#D48900')
 ass('conda clean -y --all', '【 Cleaning Conda 】', '#66ff00')
 ass('curl -Lo ~/.ipython/profile_default/startup/pantat88.py https://github.com/gutris1/segsmaker/raw/main/script/pantat88.py', '【 Gathering Magic 】', 'red')
-ass('curl -Lo ~/.ipython/profile_default/startup/00-startup.py https://github.com/gutris1/segsmaker/raw/main/script/00-startup.py', '', '')
 display(HTML('<span style="color: cyan;">【 Done 】</span>'))
 
 from IPython import get_ipython
