fdc8a503010a38134bb92f130a21496004111593
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-02-04 16:43:31 +0700

    Update zrok.py

--- ui/cui/zrok.py
+++ ui/cui/zrok.py
@@ -1,27 +1,49 @@
-import sys
 import subprocess
+import sys
 import os
+import re
+import threading
+
+plock = threading.Lock()
 
 def hitozuma(token):
     
     os.environ['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
     
-    oppai = subprocess.run(['/home/studio-lab-user/.zrok/bin/zrok', 'enable', token], 
-                            check=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    try:
+        oppai = subprocess.run(['/home/studio-lab-user/.zrok/bin/zrok', 'enable', token], 
+                                check=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
-    if oppai.returncode == 0:
-        print("[ZROK] environment enabled")
-    else:
-        if "enabled environment" in oppai.stdout:
-            print("[ZROK] environment already enabled")
+        ass = subprocess.Popen(["/home/studio-lab-user/.zrok/bin/zrok", "share", "public", "localhost:7860", "--headless"],
+                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        
+        urlp = re.compile(r'https?://[^\s]*\.zrok\.io')
+        
+        if oppai.returncode == 0:
+            print("\n[ZROK] environment enabled.")
+            
         else:
-            print(oppai.stdout)
+            if "enabled environment" in oppai.stdout:
+                print("\n[ZROK] environment already enabled.")
+            else:
+                print(oppai.stdout)
 
-    ass = "/home/studio-lab-user/.zrok/bin/zrok share public localhost:8188 --headless 2>&1 | grep 'zrok.io'"
-    subprocess.run(ass, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+        for line in ass.stdout:
+            urls = urlp.findall(line)
+            for url in urls:
+                zurl(f"[ZROK] {url}\n")
+
+        ass.wait()
+
+    except:
+        pass
+
+def zurl(url):
+    with plock:
+        print(url)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        sys.exit(1)
+        sys.exit("")
 
     hitozuma(sys.argv[1])
