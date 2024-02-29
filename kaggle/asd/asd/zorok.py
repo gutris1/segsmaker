23d0439eaa0a4439fd4a8ca40cf8c324a920af66
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-03-01 01:33:10 +0700

    .

--- kaggle/asd/asd/zorok.py
+++ kaggle/asd/asd/zorok.py
@@ -3,19 +3,11 @@ import subprocess
 
 def zzz():
     try:
-        zorok = "/kaggle/working/.zrok/bin"
+        zorok = "/kaggle/working/asd/zrok/bin"
         os.makedirs(zorok, exist_ok=True)
         subprocess.run(["curl", "-sLO", "https://github.com/openziti/zrok/releases/download/v0.4.23/zrok_0.4.23_linux_amd64.tar.gz"], check=True)
         subprocess.run(["tar", "-xzf", "zrok_0.4.23_linux_amd64.tar.gz", "-C", zorok, "--wildcards", "*zrok"], check=True)
         os.remove("zrok_0.4.23_linux_amd64.tar.gz")
-        print("""
-Now, register your ZROK account.
-Copy and Paste this command
-~/.zrok/bin/zrok invite
-into Terminal
-
-or you can watch this short tutorial https://youtu.be/prTD99GbWlQ
-""")
 
     except Exception as e:
         print(f"An error occurred: {e}")
