c783164d98e34f39abb7ecf7c39eb4d73d3e3da8
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-03-02 18:26:56 +0700

    .

--- kaggle/asd/asd/asd.py
+++ kaggle/asd/asd/asd.py
@@ -2,10 +2,11 @@ import subprocess
 import os
 
 minyak = [
-    ['rm', '-rf', '/kaggle/temp/*', '/kaggle/temp', '/kaggle/working/asd/models/Stable-diffusion', '/kaggle/working/asd/models/Lora'],
+    ['rm', '-rf', '/kaggle/temp/*', '/kaggle/temp', '/kaggle/working/asd/models/Stable-diffusion', '/kaggle/working/asd/models/Lora', '/kaggle/working/asd/outputs'],
     ['mkdir', '-p', '/kaggle/working/asd/models/ESRGAN'],
     ['ln', '-vs', '/kaggle/temp/checkpoint', '/kaggle/working/asd/models/Stable-diffusion'],
     ['ln', '-vs', '/kaggle/temp/lora', '/kaggle/working/asd/models/Lora'],
+    ['ln', '-vs', '/kaggle/temp/output', '/kaggle/working/asd/outputs'],
     ['unzip', '-o', '/kaggle/working/asd/embeddings.zip', '-d', '/kaggle/working/asd/embeddings'],
     ['rm', '/kaggle/working/asd/embeddings.zip']]
 
