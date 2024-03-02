c97f7252a46585e0d5802943b82f8b61aaa97453
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-03-03 01:25:01 +0700

    .

--- kaggle/asd/asd/asd.py
+++ kaggle/asd/asd/asd.py
@@ -14,3 +14,16 @@ minyak = [
 for tepung in minyak:
     gorengan = [os.path.expanduser(arg) for arg in tepung]
     subprocess.run(gorengan, check=True)
+
+def zzz():
+    try:
+        zorok = "/kaggle/working/asd/zrok/bin"
+        os.makedirs(zorok, exist_ok=True)
+        subprocess.run(["curl", "-sLO", "https://github.com/openziti/zrok/releases/download/v0.4.23/zrok_0.4.23_linux_amd64.tar.gz"], check=True)
+        subprocess.run(["tar", "-xzf", "zrok_0.4.23_linux_amd64.tar.gz", "-C", zorok, "--wildcards", "*zrok"], check=True)
+        os.remove("zrok_0.4.23_linux_amd64.tar.gz")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+zzz()
