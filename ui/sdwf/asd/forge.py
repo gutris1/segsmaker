5bb73db96a458e7a7f25b0e40fbb0b2a59d7a991
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-03-04 16:42:31 +0700

    Update forge.py

--- ui/sdwf/asd/forge.py
+++ ui/sdwf/asd/forge.py
@@ -1,15 +1,17 @@
-import subprocess
 import os
+import subprocess
 
-minyak = [
-    ['rm', '-rf', '~/tmp/*', '~/tmp', '~/sdwf/models/Stable-diffusion/tmp_models', '~/sdwf/models/Lora/tmp_Lora', '~/sdwf/models/ControlNet'],
-    ['mkdir', '-p', '~/sdwf/models/Lora'],
-    ['mkdir', '-p', '~/sdwf/models/ESRGAN'],
-    ['ln', '-vs', '/tmp', '~/tmp'],
-    ['ln', '-vs', '/tmp/models', '~/sdwf/models/Stable-diffusion/tmp_models'],
-    ['ln', '-vs', '/tmp/Lora', '~/sdwf/models/Lora/tmp_Lora'],
-    ['ln', '-vs', '/tmp/ControlNet', '~/sdwf/models/ControlNet']]
+jalanan = [
+    'rm -rf ~/tmp/* ~/tmp ~/sdwf/models/Stable-diffusion/tmp_models ~/sdwf/models/Lora/tmp_Lora ~/sdwf/models/ControlNet ~/sdwf/models/svd ~/sdwf/models/z123',
+    'mkdir -p ~/sdwf/models/Lora',
+    'mkdir -p ~/sdwf/models/ESRGAN',
+    'ln -vs /tmp ~/tmp',
+    'ln -vs /tmp/models ~/sdwf/models/Stable-diffusion/tmp_models',
+    'ln -vs /tmp/svd ~/sdwf/models/svd',
+    'ln -vs /tmp/z123 ~/sdwf/models/z123',
+    'ln -vs /tmp/Lora ~/sdwf/models/Lora/tmp_Lora',
+    'ln -vs /tmp/ControlNet ~/sdwf/models/ControlNet']
 
-for tepung in minyak:
-    gorengan = [os.path.expanduser(arg) for arg in tepung]
-    subprocess.run(gorengan, check=True)
+for janda in jalanan:
+    bocil = os.path.expanduser(janda)
+    subprocess.run(bocil, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
