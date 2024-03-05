8e0bc902d6ea43f3be84d2efbb21c52b771c3332
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-03-05 21:39:28 +0700

    ,

--- kaggle/asd/asd/asd.py
+++ kaggle/asd/asd/asd.py
@@ -1,25 +1,25 @@
 import subprocess
 import os
 
-minyak = [
-    ['rm', '-rf', '/kaggle/temp/*', '/kaggle/working/asd/models/Stable-diffusion', '/kaggle/working/asd/models/Lora', '/kaggle/working/asd/outputs'],
-    ['mkdir', '-p', '/kaggle/working/asd/models/ESRGAN'],
-    ['ln', '-vs', '/kaggle/temp/checkpoint', '/kaggle/working/asd/models/Stable-diffusion'],
-    ['ln', '-vs', '/kaggle/temp/lora', '/kaggle/working/asd/models/Lora'],
-    ['ln', '-vs', '/kaggle/temp/output', '/kaggle/working/asd/outputs'],
-    ['mkdir', '-p', '/kaggle/temp/checkpoint', '/kaggle/temp/lora', '/kaggle/temp/output']]
+jalanan = [
+    'rm -rf /kaggle/temp/* /kaggle/working/asd/models/Stable-diffusion /kaggle/working/asd/models/Lora /kaggle/working/asd/outputs',
+    'mkdir -p /kaggle/working/asd/models/ESRGAN',
+    'ln -vs /kaggle/temp/checkpoint /kaggle/working/asd/models/Stable-diffusion',
+    'ln -vs /kaggle/temp/lora /kaggle/working/asd/models/Lora',
+    'ln -vs /kaggle/temp/output /kaggle/working/asd/outputs',
+    'mkdir -p /kaggle/temp/checkpoint /kaggle/temp/lora /kaggle/temp/output /kaggle/temp/svd /kaggle/temp/z123']
 
-for tepung in minyak:
-    gorengan = [os.path.expanduser(arg) for arg in tepung]
-    subprocess.run(gorengan, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
+for janda in jalanan:
+    bocil = os.path.expanduser(janda)
+    subprocess.run(bocil, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    
 def zzz():
     try:
         zorok = "/kaggle/working/asd/zrok/bin"
         os.makedirs(zorok, exist_ok=True)
-        subprocess.run(["curl", "-sLO", "https://github.com/openziti/zrok/releases/download/v0.4.23/zrok_0.4.23_linux_amd64.tar.gz"], check=True)
-        subprocess.run(["tar", "-xzf", "zrok_0.4.23_linux_amd64.tar.gz", "-C", zorok, "--wildcards", "*zrok"], check=True)
-        os.remove("zrok_0.4.23_linux_amd64.tar.gz")
+        subprocess.run(["curl", "-sLO", "https://github.com/openziti/zrok/releases/download/v0.4.25/zrok_0.4.25_linux_amd64.tar.gz"], check=True)
+        subprocess.run(["tar", "-xzf", "zrok_0.4.25_linux_amd64.tar.gz", "-C", zorok, "--wildcards", "*zrok"], check=True)
+        os.remove("zrok_0.4.25_linux_amd64.tar.gz")
 
     except Exception as e:
         print(f"An error occurred: {e}")
