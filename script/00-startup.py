6726e453bedea71942d8ca3144e82cb29feccf6d
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-06-22 01:38:13 +0700

    Update 00-startup.py

--- script/00-startup.py
+++ script/00-startup.py
@@ -1,5 +1,3 @@
-import os, sys
+import sys
 
-boa = '/tmp/venv/bin/python3'
-os.environ['PYTHONPATH'] = f"{os.environ.get('PYTHONPATH', '')}:{boa}"
 sys.path.append("/home/studio-lab-user/.ipython/profile_default/startup")
