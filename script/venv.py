847f0a59b1c203244be6670a57cbc60ec6f28464
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-06-22 01:28:43 +0700

    Create venv.py

---
+++ script/venv.py
@@ -0,0 +1,40 @@
+from IPython.display import clear_output
+from IPython import get_ipython
+from pathlib import Path
+from nenen import download, say, tempe
+import os
+
+url = 'https://huggingface.co/pantat88/back_up/resolve/main/venv.tar.lz4'
+fn = Path(url).name
+tmp_ = Path('/tmp')
+venv_ = tmp_ / 'venv'
+cwd = os.getcwd()
+
+say('【{red} Checking VENV{d} 】{red}')
+
+def check(folder):
+    du = get_ipython().getoutput(f'du -s -b {folder}')
+    if du:
+        size = int(du[0].split()[0])
+        return size
+    else:
+        return 0
+
+def venv():
+    venv_.mkdir(parents=True, exist_ok=True)
+    if venv_.exists() and check(venv_) > 7 * 1024**3:
+        return
+    else:
+        say('【{red} Installing VENV{d} 】{red}')
+        os.chdir(venv_)
+        download(url)
+        get_ipython().system(f'pv {fn} | lz4 -d | tar xf -')
+        get_ipython().system(f'rm -rf {venv_ / "bin" / "pip*"}')
+        get_ipython().system(f'rm -rf {venv_ / "bin" / "python*"}')
+        os.system(f'python -m venv {venv_}')
+        get_ipython().system(f'rm -rf {fn}')
+
+tempe()
+venv()
+clear_output()
+os.chdir(cwd)
