a2778e8b521ffcf7147a46c5308b150b680ccc06
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-06-22 14:39:26 +0700

    Update 00-startup.py

--- script/00-startup.py
+++ script/00-startup.py
@@ -1,3 +1,12 @@
-import sys
+import sys, os
+from pathlib import Path
 
 sys.path.append("/home/studio-lab-user/.ipython/profile_default/startup")
+
+zrok_bin = Path('/home/studio-lab-user/.zrok/bin/zrok')
+if zrok_bin.exists():
+    if 'zrok' not in os.environ.get('PATH', '') or str(zrok_bin.parent) not in os.environ['PATH']:
+        os.system(f'chmod +x {zrok_bin}')
+        os.environ['PATH'] += os.pathsep + str(zrok_bin.parent)
+else:
+    pass
