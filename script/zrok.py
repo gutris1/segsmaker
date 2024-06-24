902181f611bad4ebd80b7313075b4e60e73b3f56
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-06-24 12:38:05 +0700

    Update zrok.py

--- script/zrok.py
+++ script/zrok.py
@@ -1,4 +1,4 @@
-import os, sys, json, re, subprocess, time
+import os, subprocess, sys, json, re
 from threading import Thread
 from pathlib import Path
 
@@ -26,21 +26,56 @@ def zrok_enable(token):
         os.system(f'zrok enable {token}')
 
 def launch(zrok):
-    webui = subprocess.Popen(['/tmp/venv/bin/python3', 'launch.py'] + sys.argv[2:])
-    zrok.append(subprocess.Popen(["zrok", "share", "public", "localhost:7860", "--headless"],
-                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True))
+    with open('launch.txt', 'w') as log_file:
+        webui = subprocess.Popen([
+            '/tmp/venv/bin/python3',
+            'launch.py'] + sys.argv[2:],
+            stdout=subprocess.PIPE,
+            stderr=sys.stdout,
+            text=True)
 
-    webui.wait()
-    zrok[0].terminate()
+        zrok_process = subprocess.Popen([
+            "zrok",
+            "share",
+            "public",
+            "localhost:7860",
+            "--headless"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True)
+
+        zrok.append(zrok_process)
+
+        local_url = False
+        for line in webui.stdout:
+            print(line, end='')
+            if not local_url:
+                log_file.write(line)
+                log_file.flush()
+                if 'Running on local URL' in line:
+                    local_url = True
+
+        webui.wait()
+        zrok[0].terminate()
 
 def zrok_url(zrok):
-    time.sleep(2)
-    get_url = re.compile(r'https?://[^\s]*\.zrok\.io')
+    zrok_io = re.compile(r'https?://[^\s]*\.zrok\.io')
+    zrok_url = None
+
+    while not zrok:
+        pass
+
     for line in zrok[0].stdout:
-        urls = get_url.findall(line)
-        for url in urls:
-            print(f"\n【ZROK】{url}\n")
+        urls = zrok_io.findall(line)
+        if urls:
+            zrok_url = urls[0]
             break
+
+    while True:
+        with open('launch.txt', 'r') as file:
+            if any('Running on local URL' in line for line in file):
+                print(f'\n【ZROK】{zrok_url}')
+                return
 
 try:
     if len(sys.argv) < 2:
