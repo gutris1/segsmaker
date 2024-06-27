194fd54804fd332d05b8bd7845f8f049e3c0f3e8
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-06-27 18:36:03 +0700

    Delete script/comfyuickpt.py

--- script/comfyuickpt.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import os
-
-comfyui = Path.home() / "ComfyUI"
-if comfyui.exists():
-    ckpt = comfyui / "models/checkpoints_symlink"
-    src = comfyui / "models/checkpoints"
-    os.system(f"unlink {ckpt}")
-    os.system(f"ln -vs {src} {ckpt}")
