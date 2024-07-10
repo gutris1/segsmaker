dd9c5d2b82cbf707eb21485db1fa5656a4b2f5ca
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-07-10 08:22:12 +0700

    🍇

--- script/multi/marking.py
+++ script/multi/marking.py
@@ -4,27 +4,32 @@ import json
 
 home = Path.home()
 src = home / '.gutris1'
-marc = src / 'marking.json'
+mark = src / 'marking.json'
 
-get_ipython().magic('reset -f')
+def del_var():
+    variables = ['webui', 'models', 'webui_output', 'extensions', 'embeddings',
+                 'vae', 'ckpt', 'tmp_ckpt', 'lora', 'tmp_lora', 'forge_svd']
+
+    for var in variables:
+        if var in globals():
+            del globals()[var]
 
 def get_name(path):
     with open(path, 'r') as file:
         value = json.load(file)
-
         return value.get('ui', None)
 
-if marc.exists():
-    ui = get_name(marc)
+if mark.exists():
+    del_var()
+    
+    ui = get_name(mark)
 
     if ui == 'A1111':
         webui = home / 'asd'
         models = webui / 'models'
-
         webui_output = webui / 'outputs'
         extensions = webui / 'extensions'
         embeddings = webui / 'embeddings'
-
         vae = models / 'VAE'
         ckpt = models / 'Stable-diffusion'
         tmp_ckpt = models / 'Stable-diffusion/tmp_ckpt'
@@ -34,27 +39,22 @@ if marc.exists():
     elif ui == 'Forge':
         webui = home / 'forge'
         models = webui / 'models'
-
         webui_output = webui / 'output'
         extensions = webui / 'extensions'
         embeddings = webui / 'embeddings'
-
         vae = models / 'VAE'
         ckpt = models / 'Stable-diffusion'
         tmp_ckpt = models / 'Stable-diffusion/tmp_ckpt'
         lora = models / 'Lora'
         tmp_lora = models / 'Lora/tmp_lora'
-        
         forge_svd = models / 'svd'
 
     elif ui == 'ComfyUI':
         webui = home / 'ComfyUI'
         models = webui / 'models'
-
         webui_output = webui / 'output'
         extensions = webui / 'custom_nodes'
         embeddings = models / 'embeddings'
-
         vae = models / 'vae'
         ckpt = models / 'checkpoints'
         tmp_ckpt = models / 'checkpoints/tmp_ckpt'
