45d120c8ec5c33e348fbd17a0ac7b157107f7d80
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-07-22 18:09:19 +0700

    Update marking.py

--- script/multi/marking.py
+++ script/multi/marking.py
@@ -1,3 +1,4 @@
+from IPython.core.magic import register_line_magic
 from pathlib import Path
 from nenen88 import tempe
 import json
@@ -32,11 +33,37 @@ def get_name(path):
         value = json.load(file)
         return value.get('ui', None)
 
+def get_webui_paths():
+    ui = get_name(marked)
+    webui_paths = {
+        'A1111': 'asd',
+        'Forge': 'forge',
+        'ComfyUI': 'ComfyUI'
+    }
+
+    webui = home / webui_paths[ui]
+    webui_output = webui / 'outputs' if ui == 'A1111' else webui / 'output'
+    return webui, webui_output
+
+@register_line_magic
+def clear_output_images(line):
+    _, webui_output = get_webui_paths()
+    get_ipython().system(f"rm -rf {webui_output}/* {home / '.cache/*'}")
+    get_ipython().run_line_magic('cd', '-q ~')
+    print('output cleared.')
+
+@register_line_magic
+def uninstall_webui(line):
+    webui, _ = get_webui_paths()
+    get_ipython().system(f"rm -rf {webui} {home / 'tmp'} {home / '.cache/*'}")
+    get_ipython().run_line_magic('cd', '-q ~')
+    print('webui uninstalled.')
+
 def set_paths(ui):
     webui_paths = {
         'A1111': ('asd', 'extensions', 'embeddings', 'VAE', 'Stable-diffusion', 'Lora'),
         'Forge': ('forge', 'extensions', 'embeddings', 'VAE', 'Stable-diffusion', 'Lora'),
-        'ComfyUI': ('ComfyUI', 'custom_nodes', 'models/embeddings', 'vae', 'checkpoints', 'loras')
+        'ComfyUI': ('ComfyUI', 'custom_nodes', 'embeddings', 'vae', 'checkpoints', 'loras')
     }
 
     if ui in webui_paths:
@@ -45,7 +72,7 @@ def set_paths(ui):
         models = webui / 'models'
         webui_output = webui / 'outputs' if ui == 'A1111' else webui / 'output'
         extensions = webui / ext
-        embeddings = models / emb if ui != 'ComfyUI' else webui / emb
+        embeddings = models / emb if ui == 'ComfyUI' else webui / emb
         vae = models / v
         ckpt = models / c
         lora = models / l
@@ -56,8 +83,7 @@ if marked.exists():
 
     ui = get_name(marked)
     webui, models, webui_output, extensions, embeddings, vae, ckpt, lora = set_paths(ui)
-    
-    delete_install = f"rm -rf {webui} {home / 'tmp'} {home / '.cache/*'}"
+
     controlnet_models = webui / 'asd/controlnet.py'
     forge_svd = tmp / 'svd' if ui == 'Forge' else None
     tmp_ckpt = tmp / 'ckpt'
