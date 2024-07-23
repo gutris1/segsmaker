077efddd54b6490ec402d35bf69927149e02540d
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-07-24 06:48:42 +0700

    Update setup.py

--- ui/sd/asd/setup.py
+++ ui/sd/asd/setup.py
@@ -1,5 +1,5 @@
 asd = (
-    'The individual notebooks have been discontinued\nGet the multi-notebook named '
+    'Discontinued\nGet the multi-notebook named '
     'Segsmaker.ipynb instead\nhttps://github.com/gutris1/segsmaker'
 )
 
