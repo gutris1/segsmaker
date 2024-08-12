ae46588b7b0ad27c88bb7461d0d743246b38f8ae
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-08-12 22:58:59 +0700

    Update zrok.py

--- script/zrok.py
+++ script/zrok.py
@@ -1,36 +1,39 @@
-import os, sys, json
+import subprocess, sys, os, logging
 from pathlib import Path
 
-def zrok_enable(token):
-    zrok = Path('/home/studio-lab-user/.zrok')
-    if not zrok.exists():
-        print("ZROK is not installed.")
-        return
+def logging_launch():
+    log_file = Path('segsmaker.log')
+    log_file.write_text('A1111 or Forge\n')
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.INFO,
+        format="{message}", style="{"
+    )
+    return logging.getLogger()
 
-    env = zrok / 'environment.json'
-    if env.exists():
-        with open(env, 'r') as f:
-            value = json.load(f)
-            zrok_token = value.get('zrok_token')
-
-        if zrok_token == token:
-            pass
-        else:
-            os.system('zrok disable')
-            os.system(f'zrok enable {token}')
-    else:
-        os.system(f'zrok enable {token}')
-
-def zrok():
+def launch(logger):
     if 'LD_PRELOAD' not in os.environ:
         os.environ['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
 
-    os.system(f"/tmp/venv/bin/python3 launch.py {' '.join(sys.argv[2:])}")
+    webui = subprocess.Popen(['/tmp/venv/bin/python3', 'launch.py'] + sys.argv[1:],
+                             stdout=subprocess.PIPE, stderr=sys.stdout, text=True)
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.exit(1)
+    local_url = False
+    for line in webui.stdout:
+        print(line, end='')
+        logger.info(line.strip())
+        if not local_url:
+            if 'Running on local URL' in line:
+                local_url = True
+                for handler in logger.handlers[:]:
+                    handler.flush()
+                    handler.close()
+                    logger.removeHandler(handler)
+    webui.wait()
 
-    token = sys.argv[1]
-    zrok_enable(token)
-    zrok()
+if __name__ == '__main__':
+    logger = logging_launch()
+    try:
+        launch(logger)
+    except KeyboardInterrupt:
+        pass
