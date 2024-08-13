e3cb4f37046bb890eecb795d83cd985c95875a68
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-08-13 17:24:33 +0700

    Update pinggy.py

--- ui/cui/pinggy.py
+++ ui/cui/pinggy.py
@@ -1,48 +1,39 @@
-import os, sys, subprocess, time
-from threading import Thread
+import subprocess, sys, os, logging
 from pathlib import Path
 
-R = '\033[0m'
-O = '\033[38;5;208m'
-T = f'{O}▶{R} PINGGY {O}:{R}'
+def logging_launch():
+    log_file = Path('segsmaker.log')
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.INFO,
+        format="{message}", style="{",
+        filemode='a'
+    )
+    return logging.getLogger()
 
-if 'LD_PRELOAD' not in os.environ:
-    os.environ['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
+def launch(logger):
+    if 'LD_PRELOAD' not in os.environ:
+        os.environ['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
 
-def launch():
-    webui = subprocess.Popen(['/tmp/venv/bin/python3', 'main.py'] + sys.argv[1:])
-    ssh = subprocess.Popen([
-        'ssh',
-        '-o',
-        'StrictHostKeyChecking=no',
-        '-p',
-        '80',
-        '-R0:localhost:8188',
-        'a.pinggy.io'],
-        stdout=open('pinggy.txt', 'w'))
+    webui = subprocess.Popen(['/tmp/venv/bin/python3', 'main.py'] + sys.argv[1:],
+                             stdout=subprocess.PIPE, stderr=sys.stdout, text=True)
 
+    local_url = False
+    for line in webui.stdout:
+        print(line, end='')
+        logger.info(line.strip())
+        if not local_url:
+            if 'Torch version:' in line:
+                local_url = True
+                for handler in logger.handlers[:]:
+                    handler.flush()
+                    handler.close()
+                    logger.removeHandler(handler)
     webui.wait()
-    ssh.terminate()
 
-def pinggy():
-    time.sleep(2)
-    with open('pinggy.txt', 'r') as file:
-        for line in file:
-            if 'http:' in line and '.pinggy.link' in line:
-                url = line[line.find('http:'):line.find('.pinggy.link') + len('.pinggy.link')]
-                print(f'{T} {url}\n')
-                return
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    logger = logging_launch()
     try:
-        app = Thread(target=launch)
-        url = Thread(target=pinggy)
-
-        app.start()
-        url.start()
-
-        app.join()
-        url.join()
-
+        launch(logger)
     except KeyboardInterrupt:
         pass
