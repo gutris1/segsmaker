9d0577dd66c6cb599602a45f632f3890a662cbca
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-10-01 17:56:29 +0700

    Update 00-startup.py

--- script/00-startup.py
+++ script/00-startup.py
@@ -9,7 +9,6 @@ zrok_bin = home / '.zrok/bin/zrok'
 startup = home / '.ipython/profile_default/startup'
 
 sys.path.append(str(startup))
-
 if zrok_bin.exists():
     if 'zrok' not in os.environ.get('PATH', '') or str(zrok_bin.parent) not in os.environ['PATH']:
         zrok_bin.chmod(0o755)
