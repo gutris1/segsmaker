61b794f730667c540b0f7c8f900b4b023147bba0
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-10-31 22:58:23 +0700

    Update magic_bell.py

--- kaggle/script/magic_bell.py
+++ kaggle/script/magic_bell.py
@@ -8,53 +8,58 @@ from IPython.display import Audio, display
 
 class _InvisibleAudio(Audio):
     """
-    An invisible (`display: none`) `Audio` element with a volume control,
-    which removes itself when finished playing.
+    An invisible (`display: none`) `Audio` element which removes itself when finished playing.
+    Taken from https://stackoverflow.com/a/50648266.
     """
-    
-    def __init__(self, *args, volume=1.0, **kwargs):
-        self.volume = volume
-        super().__init__(*args, **kwargs)
 
     def _repr_html_(self) -> str:
         audio = super()._repr_html_()
         audio = audio.replace(
-            "<audio",
-            f'<audio volume="{self.volume}" onended="this.parentNode.removeChild(this)"'
+            "<audio", '<audio onended="this.parentNode.removeChild(this)"'
         )
         return f'<div style="display:none">{audio}</div>'
 
+
 @magics_class
 class NotificationMagics(Magics):
+    """
+    Inspired by https://stackoverflow.com/a/50648266.
+
+    If a bell.wav file exists in the same directory as this file, that will be used as
+    the default sound. Otherwise, the default is the wav file at `DEFAULT_URL`.
+    Providing a URL to the magic overrides either of these defaults.
+    """
+
     SOUND_FILE = str(Path(__file__).parent.resolve() / "bell.wav")
     DEFAULT_URL = "https://freewavesamples.com/files/E-Mu-Proteus-FX-CosmoBel-C3.wav"
 
     @magic_arguments()
-    @argument("-u", "--url", default=SOUND_FILE, help="URL of audio file to play.")
-    @argument("-m", "--mute", default=SOUND_FILE, help="Play audio muted (for caching).")
-    @argument("line_code", nargs="*", help="Code to execute on the same line.")
+    @argument(
+        "-u", "--url", default=SOUND_FILE, help="URL of audio file to play.",
+    )
+    @argument(
+        "line_code",
+        nargs="*",
+        help="Other code on the line will be executed, unless this is called as a cell magic.",
+    )
     @line_cell_magic
     def notify(self, line: str, cell: Optional[str] = None):
         args = parse_argstring(self.notify, line)
         code = cell if cell else " ".join(args.line_code)
-
+        
         try:
             ret = self.shell.ex(code)
         finally:
             maybe_url = args.url
-            is_muted = args.mute
-            volume = 0 if is_muted else 1.0
-
             if maybe_url == self.SOUND_FILE:
                 if Path(self.SOUND_FILE).is_file():
                     audio = _InvisibleAudio(filename=maybe_url, autoplay=True)
                 else:
                     audio = _InvisibleAudio(url=self.DEFAULT_URL, autoplay=True)
+            elif Path(maybe_url).is_file():
+                audio = _InvisibleAudio(filename=maybe_url, autoplay=True)
             else:
                 audio = _InvisibleAudio(url=maybe_url, autoplay=True)
             display(audio)
-
         return ret
-
-
 get_ipython().register_magics(NotificationMagics)
