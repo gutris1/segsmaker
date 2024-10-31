ab6132fda435600bbc4053599f932bbdad00c686
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-10-31 20:15:23 +0700

    Update bell_magic.py

--- kaggle/script/bell_magic.py
+++ kaggle/script/bell_magic.py
@@ -8,10 +8,6 @@ import requests
 
 
 class _InvisibleAudio(Audio):
-    """
-    An invisible (`display: none`) `Audio` element which removes itself when finished playing.
-    """
-
     def _repr_html_(self) -> str:
         audio = super()._repr_html_()
         audio = audio.replace(
@@ -22,55 +18,61 @@ class _InvisibleAudio(Audio):
 
 @magics_class
 class NotificationMagics(Magics):
-    """
-    Adds a magic to IPython which will play a given sound when a cell finishes running.
-    """
-
-    SOUND_DIR = Path(__file__).parent.resolve()
-    FILE_MAP = {
-        "1": SOUND_DIR / "1.wav",
-        "2": SOUND_DIR / "2.wav",
-        "3": SOUND_DIR / "3.wav",
+    SOUND_DIR = Path(__file__).parent.resolve() / "SOUND"
+    SOUND_FILES = {
+        "1": "https://huggingface.co/pantat88/ui/resolve/main/1.wav",
+        "2": "https://huggingface.co/pantat88/ui/resolve/main/2.wav",
+        "3": "https://huggingface.co/pantat88/ui/resolve/main/3.wav"
     }
+    
+    SOUND_FILE = str(SOUND_DIR / "bell.wav")
     DEFAULT_URL = "https://freewavesamples.com/files/E-Mu-Proteus-FX-CosmoBel-C3.wav"
 
     def __init__(self, shell):
         super().__init__(shell)
-        self._ensure_audio_files()
+        self._download_sounds()
 
-    def _ensure_audio_files(self):
-        """Download the sound files if they are not present."""
-        for num, file_path in self.FILE_MAP.items():
-            if not file_path.exists():
-                url = f"https://huggingface.co/pantat88/ui/resolve/main/{num}.wav"
+    def _download_sounds(self):
+        for key, url in self.SOUND_FILES.items():
+            file_path = self.SOUND_DIR / f"{key}.wav"
+            if not file_path.is_file():
                 response = requests.get(url)
-                file_path.write_bytes(response.content)
+                with open(file_path, 'wb') as f:
+                    f.write(response.content)
 
     @magic_arguments()
     @argument(
-        "-u", "--url", default="1", help="Specify sound by number (1, 2, or 3).",
+        "-u", "--url", default=SOUND_FILE, help="URL of audio file to play.",
     )
     @argument(
         "line_code",
         nargs="*",
-        help="Other code on the line will be executed unless called as a cell magic.",
+        help="Other code on the line will be executed, unless this is called as a cell magic.",
     )
     @line_cell_magic
     def notify(self, line: str, cell: Optional[str] = None):
         args = parse_argstring(self.notify, line)
 
         code = cell if cell else " ".join(args.line_code)
-        ret = None
-        if code:
+        try:
             ret = self.shell.ex(code)
+        finally:
+            if args.line_code and args.line_code[0] in self.SOUND_FILES:
+                sound_key = args.line_code[0]
+                audio_file = self.SOUND_DIR / f"{sound_key}.wav"
+                audio = _InvisibleAudio(filename=str(audio_file), autoplay=True)
+            else:
+                maybe_url = args.url
+                if maybe_url == self.SOUND_FILE:
+                    if Path(self.SOUND_FILE).is_file():
+                        audio = _InvisibleAudio(filename=maybe_url, autoplay=True)
+                    else:
+                        audio = _InvisibleAudio(url=self.DEFAULT_URL, autoplay=True)
+                else:
+                    audio = _InvisibleAudio(url=maybe_url, autoplay=True)
+            display(audio)
 
-        sound_file = self.FILE_MAP.get(args.url, None)
-        if sound_file and sound_file.exists():
-            audio = _InvisibleAudio(filename=str(sound_file), autoplay=True)
-        else:
-            audio = _InvisibleAudio(url=self.DEFAULT_URL, autoplay=True)
-        
-        display(audio)
         return ret
+
 
 get_ipython().register_magics(NotificationMagics)
