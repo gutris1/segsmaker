bb99c9a3c3e6b9c30c22adfce576f3e80278e1c8
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-11-04 16:15:48 +0700

    Create segsmaker.py

---
+++ kaggle/script/segsmaker.py
@@ -0,0 +1,35 @@
+import json, os, logging, sys
+from pathlib import Path
+from cupang import Tunnel as Alice_Zuberg
+from KANDANG import HOMEPATH
+
+MARKED = Path(HOMEPATH) / 'gutris1/marking.json'
+pw = '82a973c04367123ae98bd9abdf80d9eda9b910e2'
+
+def shut_up(launch_args):
+    config = json.load(MARKED.open('r')) if MARKED.exists() else {}
+    ui = config.get('ui')
+
+    port = 8188 if ui == 'ComfyUI' else 7860
+    launch = 'main.py' if ui == 'ComfyUI' else 'launch.py'
+
+    if ui != 'ComfyUI':
+        launch_args += f' --encrypt-pass={pw}'
+
+    tunnel = f'cl tunnel --url localhost:{port}'
+
+    Alice_Synthesis_Thirty = Alice_Zuberg(port)
+    Alice_Synthesis_Thirty.logger.setLevel(logging.DEBUG)
+    Alice_Synthesis_Thirty.add_tunnel(command=tunnel, name='Cloudflared', pattern=r"[\w-]+\.trycloudflare\.com")
+
+    with Alice_Synthesis_Thirty:
+        cmd = f'/kaggle/venv/bin/python3 {launch} {launch_args}'
+        os.system(cmd)
+
+if __name__ == '__main__':
+    launch_args = ' '.join(sys.argv[1:])
+
+    try:
+        shut_up(launch_args)
+    except KeyboardInterrupt:
+        pass
