0e53b2f599ad06f081b7234770f7480acce39bcc
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-11-07 13:44:38 +0700

    Update SDTrainer.py

--- script/SM/SDTrainer.py
+++ script/SM/SDTrainer.py
@@ -43,9 +43,9 @@ def webui_req():
         subprocess.run(shlex.split(lines), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     scripts = [
-        f"https://github.com/gutris1/segsmaker/raw/main/ui/trainer/launch.py {WEBUI}",
-        f"https://github.com/gutris1/segsmaker/raw/main/script/venv.py {WEBUI}",
-        f"https://github.com/gutris1/segsmaker/raw/main/script/multi/segsmaker.py {WEBUI}"]
+        f"https://github.com/gutris1/segsmaker/raw/main/config/sdtrainer/launch.py {WEBUI}",
+        f"https://github.com/gutris1/segsmaker/raw/main/script/SM/venv.py {WEBUI}",
+        f"https://github.com/gutris1/segsmaker/raw/main/script/SM/segsmaker.py {WEBUI}"]
 
     for items in scripts:
         download(items)
@@ -120,9 +120,9 @@ def webui_widgets():
             get_ipython().system("git fetch --tags")
 
         x = [
-            f"https://github.com/gutris1/segsmaker/raw/main/ui/trainer/launch.py {WEBUI}",
-            f"https://github.com/gutris1/segsmaker/raw/main/script/venv.py {WEBUI}",
-            f"https://github.com/gutris1/segsmaker/raw/main/script/multi/segsmaker.py {WEBUI}"
+            f"https://github.com/gutris1/segsmaker/raw/main/config/sdtrainer/launch.py {WEBUI}",
+            f"https://github.com/gutris1/segsmaker/raw/main/script/SM/venv.py {WEBUI}",
+            f"https://github.com/gutris1/segsmaker/raw/main/script/SM/multi/segsmaker.py {WEBUI}"
         ]
         
         print()
