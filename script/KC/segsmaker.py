9c3a5698765a9a49e4c3582d43c33a60340c22a5
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-11-10 15:25:16 +0700

    Update segsmaker.py

--- script/KC/segsmaker.py
+++ script/KC/segsmaker.py
@@ -14,7 +14,7 @@ def shut_up(launch_args, skip_comfyui_check):
     ui = config.get('ui')
 
     port = 8188 if ui == 'ComfyUI' else 7860
-    launch = 'main.py' if ui == 'ComfyUI' else 'launch.py'
+    launcher = 'main.py' if ui == 'ComfyUI' else 'launch.py'
 
     if ui != 'ComfyUI':
         launch_args += f' --enable-insecure-extension-access --disable-console-progressbars --theme dark --encrypt-pass={pw}'
@@ -30,7 +30,7 @@ def shut_up(launch_args, skip_comfyui_check):
     Alice_Synthesis_Thirty.add_tunnel(command=tunnel, name='Cloudflared', pattern=r"[\w-]+\.trycloudflare\.com")
 
     with Alice_Synthesis_Thirty:
-        cmd = f'{py} {launch} {launch_args}'
+        cmd = f'{py} {launcher} {launch_args}'
         get_ipython().system(cmd)
 
 if __name__ == '__main__':
