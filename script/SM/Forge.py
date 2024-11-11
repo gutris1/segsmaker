3bfe05df7f0adaaeb99a8eba043ac77c68a62130
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-11-11 22:33:23 +0700

    Update Forge.py

--- script/SM/Forge.py
+++ script/SM/Forge.py
@@ -231,8 +231,7 @@ def webui_widgets():
             ('ComfyUI', HOME / 'ComfyUI'),
             ('ReForge', HOME / 'ReForge'),
             ('FaceFusion', HOME / 'FaceFusion'),
-            ('SDTrainer', HOME / 'SDTrainer'),
-            ('KohyaSS', HOME / 'KohyaSS')
+            ('SDTrainer', HOME / 'SDTrainer')
         ]
         
         for ui_name, path in webui_list:
