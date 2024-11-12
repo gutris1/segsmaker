8d11381fcc3ac3173f64863ffe22719257ad8341
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-11-12 22:51:18 +0700

    Update Launcher.py

--- script/SM/Launcher.py
+++ script/SM/Launcher.py
@@ -1,12 +1,14 @@
 import subprocess, sys, os, time, shlex, json
 from pathlib import Path
 
+
 MARK = Path.home() / '.gutris1/marking.json'
 
 config = json.load(MARK.open('r'))
 tunnel = config.get('tunnel')
 ui = config.get('ui')
 cwd = Path.cwd()
+
 
 def launch():
     os.environ['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
@@ -22,6 +24,22 @@ def launch():
 
     os.system(cmd)
 
+
+def swarmui_launch():
+    os.environ['PATH'] = '/tmp/venv/bin:' + os.environ['PATH']
+    os.environ['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
+    os.environ['LD_LIBRARY_PATH'] = '/tmp/venv/lib:/home/studio-lab-user/.conda/envs/default/lib:' + os.environ.get('LD_LIBRARY_PATH', '')
+
+    os.environ['SWARMPATH'] = str(cwd)
+    os.environ['SWARM_NO_VENV'] = 'true'
+
+    os.system('pip install -q rembg')
+    os.system('git pull')
+
+    cmd = "bash ./launch-linux.sh --launch_mode none"
+    subprocess.run(shlex.split(cmd), stdout=sys.stdout, stderr=sys.stdout, check=True)
+
+
 def sdtrainer_launch():
     os.environ['MPLBACKEND'] = 'gtk3agg'
     os.environ['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
@@ -29,6 +47,7 @@ def sdtrainer_launch():
 
     cmd = f'/tmp/venv-sd-trainer/bin/python3 gui.py ' + ' '.join(sys.argv[1:])
     os.system(cmd)
+
 
 def facefusion_launch():
     os.environ['MPLBACKEND'] = 'gtk3agg'
@@ -52,12 +71,15 @@ def facefusion_launch():
 
     webui.wait()
 
+
 if __name__ == '__main__':
     try:
         if ui == 'FaceFusion':
             facefusion_launch()
         elif ui == 'SDTrainer':
             sdtrainer_launch()
+        elif ui == 'SwarmUI':
+            swarmui_launch()
         else:
             launch()
     except KeyboardInterrupt:
