6d026952c4858ce7d9496fa70ce8874f9057f5f6
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-01-15 22:44:12 +0700

    Update venv.py

--- script/KC/venv.py
+++ script/KC/venv.py
@@ -1,21 +1,42 @@
 from IPython import get_ipython
 from pathlib import Path
 from nenen88 import tempe, say, download
-from KANDANG import HOMEPATH, TEMPPATH, VENVPATH, BASEPATH, ENVNAME
-import subprocess, os, shlex, errno
+from KANDANG import HOMEPATH, VENVPATH, BASEPATH
+import subprocess
+import shlex
+import errno
+import json
+import os
 
 HOME = Path(HOMEPATH)
-tmp = Path(TEMPPATH)
-vnv = Path(VENVPATH)
+VNV = Path(VENVPATH)
 
-url = 'https://huggingface.co/pantat88/back_up/resolve/main/venv-torch241-cu121.tar.lz4'
-fn = Path(url).name
+SyS = get_ipython().system
+CD = os.chdir
+
+def venv_check(ui):
+    fp = Path(BASEPATH) / "vnv.json"
+    dt = {"venv": ui}
+
+    if fp.exists():
+        with fp.open("r") as f:
+            v = json.load(f)
+
+        if v.get("venv") != ui:
+            if VNV.exists():
+                SyS(f'rm -rf {VNV}/* {VNV}')
+
+            v["venv"] = ui
+            dt = v
+
+    with fp.open("w") as f:
+        json.dump(dt, f, indent=4)
 
 def she_bang():
-    vnv_bin = vnv / 'bin'
+    vnv_bin = VNV / 'bin'
     old_shebang = b'#!/home/studio-lab-user/tmp/venv/bin/python3\n'
-    new_shebang = f"#!{vnv}/bin/python3\n"
-    
+    new_shebang = f"#!{VNV}/bin/python3\n"
+
     for script in vnv_bin.glob('*'):
         if script.is_file():
             try:
@@ -32,23 +53,33 @@ def she_bang():
                     pass
 
 def venv_install():
-    if vnv.exists():
-        get_ipython().system(f'rm -rf {vnv}/* {vnv}')
+    MD = HOME / 'gutris1/marking.json'
+    config = json.load(MD.open('r'))
+    ui = config.get('ui')
 
-    os.chdir(BASEPATH)
+    venv_check(ui)
+
+    url = 'https://huggingface.co/pantat88/back_up/resolve/main/venv-torch241-cu121.tar.lz4'
+    fn = Path(url).name
+
     say('<br>【{red} Installing VENV{d} 】{red}')
+    CD(BASEPATH)
     download(url)
 
-    get_ipython().system(f'pv {fn} | lz4 -d | tar xf -')
+    SyS(f'pv {fn} | lz4 -d | tar xf -')
     Path(fn).unlink()
 
-    get_ipython().system(f'rm -rf {vnv}/bin/pip* {vnv}/bin/python*')
+    SyS(f'rm -rf {VNV}/bin/pip* {VNV}/bin/python*')
 
     n = [
-        f'python3 -m venv {vnv}',
-        f'{vnv}/bin/python3 -m pip install -U --force-reinstall pip',
-        f'{vnv}/bin/python3 -m pip install ipykernel'
+        f'python3 -m venv {VNV}',
+        f'{VNV}/bin/python3 -m pip install -U --force-reinstall pip',
+        f'{VNV}/bin/python3 -m pip install ipykernel',
+        f'{VNV}/bin/python3 -m pip uninstall -y ngrok pyngrok'
     ]
+
+    if ui == 'Forge':
+        n.append(f'{VNV}/bin/python3 -m pip uninstall -y transformers')
 
     for p in n:
         subprocess.run(shlex.split(p), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -56,4 +87,4 @@ def venv_install():
 tempe()
 venv_install()
 she_bang()
-os.chdir(HOME)
+CD(HOME)
