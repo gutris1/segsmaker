4d8756ce5ae9f3c07d4935de33c37fd8f5094882
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-01-18 16:38:27 +0700

    Update Launcher.py

--- script/SM/Launcher.py
+++ script/SM/Launcher.py
@@ -1,66 +1,72 @@
-import subprocess, sys, os, time, shlex, json
 from pathlib import Path
+import matplotlib
+import subprocess
+import shlex
+import json
+import time
+import sys
+import os
 
 MARK = Path.home() / '.gutris1/marking.json'
-
 config = json.load(MARK.open('r'))
 tunnel = config.get('tunnel')
 ui = config.get('ui')
 cwd = Path.cwd()
 
-os.environ['PYTHONWARNINGS'] = 'ignore'
+iRON = os.environ
+SyS = os.system
+
+def setENV():
+    iRON['PYTHONWARNINGS'] = 'ignore'
+
+    def setVAR(var, new, value):
+        current_value = iRON.get(var, '')
+        if new not in current_value:
+            iRON[var] = new + (':' + current_value if current_value else '')
+
+    if ui == 'SDTrainer':
+        iRON['MPLBACKEND'] = 'gtk3agg'
+        setVAR('LD_PRELOAD', '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4', 'LD_PRELOAD')
+
+    elif ui == 'FaceFusion':
+        iRON['MPLBACKEND'] = 'gtk3agg'
+        iRON.pop('LD_PRELOAD', None)
+        iRON.pop('LD_LIBRARY_PATH', None)
+        iRON['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libcublasLt.so.12:' + \
+                                  '/home/studio-lab-user/.conda/envs/default/lib/libcublas.so.12:' + \
+                                  '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
+        iRON['LD_LIBRARY_PATH'] = '/home/studio-lab-user/.conda/envs/default/lib'
+
+    elif ui == 'SwarmUI':
+        setVAR('PATH', '/tmp/venv/bin', '/tmp/venv/bin')
+        setVAR('LD_LIBRARY_PATH', '/tmp/venv/lib:/home/studio-lab-user/.conda/envs/default/lib', 'LD_LIBRARY_PATH')
+        iRON['SWARMPATH'] = str(cwd)
+        iRON['SWARM_NO_VENV'] = 'true'
+
+    else:
+        setVAR('PATH', '/tmp/venv/bin', '/tmp/venv/bin')
+        setVAR('LD_PRELOAD', '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4', 'LD_PRELOAD')
 
 def Launch():
-    os.environ['PATH'] = '/tmp/venv/bin:' + os.environ['PATH']
-    os.environ['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
+    launcher = 'main.py' if ui == 'ComfyUI' else 'launch.py'
 
     if ui == 'SwarmUI':
-        os.environ['LD_LIBRARY_PATH'] = '/tmp/venv/lib:/home/studio-lab-user/.conda/envs/default/lib:' + os.environ.get('LD_LIBRARY_PATH', '')
-        os.environ['SWARMPATH'] = str(cwd)
-        os.environ['SWARM_NO_VENV'] = 'true'
-
-        os.system('pip install -q rembg')
-        os.system('git pull -q')
-
+        SyS('pip install -q rembg')
+        SyS('git pull -q')
         cmd = 'bash ./launch-linux.sh ' + ' '.join(sys.argv[1:])
-    
     else:
-        launcher = 'main.py' if ui == 'ComfyUI' else 'launch.py'
-
         if ui in ['A1111', 'Forge', 'ReForge']:
-            if tunnel == 'Pinggy':
-                timer = cwd / "asd" / "pinggytimer.txt"
-                end_time = int(time.time()) + 3600
-                os.system(f"echo -n {end_time} > {timer}")
-
+            timer = cwd / "asd/pinggytimer.txt"
+            end_time = int(time.time()) + 3600
+            SyS(f"echo -n {end_time} > {timer}")
         cmd = f'python3 {launcher} ' + ' '.join(sys.argv[1:])
-
-    os.system(cmd)
-
+    SyS(cmd)
 
 def sdtrainer_launch():
-    os.environ['MPLBACKEND'] = 'gtk3agg'
-    os.environ['LD_PRELOAD'] = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
-    import matplotlib
-
     cmd = '/tmp/venv-sd-trainer/bin/python3 gui.py ' + ' '.join(sys.argv[1:])
-    os.system(cmd)
-
+    SyS(cmd)
 
 def facefusion_launch():
-    os.environ['MPLBACKEND'] = 'gtk3agg'
-    import matplotlib
-
-    os.environ.pop('LD_PRELOAD', None)
-    os.environ.pop('LD_LIBRARY_PATH', None)
-
-    os.environ['LD_PRELOAD'] = (
-        '/home/studio-lab-user/.conda/envs/default/lib/libcublasLt.so.12:' +
-        '/home/studio-lab-user/.conda/envs/default/lib/libcublas.so.12:' +
-        '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
-    )
-    os.environ['LD_LIBRARY_PATH'] = '/home/studio-lab-user/.conda/envs/default/lib:' + os.environ.get('LD_LIBRARY_PATH', '')
-
     cmd = f"source activate default && /tmp/venv-fusion/bin/python3 facefusion.py run {' '.join(shlex.quote(arg) for arg in sys.argv[1:])}"
     webui = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=sys.stdout, text=True, shell=True, executable="/bin/bash")
 
@@ -69,9 +75,10 @@ def facefusion_launch():
 
     webui.wait()
 
-
 if __name__ == '__main__':
     try:
+        setENV()
+
         if ui == 'FaceFusion':
             facefusion_launch()
         elif ui == 'SDTrainer':
