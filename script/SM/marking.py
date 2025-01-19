8281f2bc7c99baa41ca63cae24566740c8cb7358
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-01-19 20:54:22 +0700

    Update marking.py

--- script/SM/marking.py
+++ script/SM/marking.py
@@ -2,63 +2,40 @@ from IPython.core.magic import register_line_magic
 from IPython import get_ipython
 from pathlib import Path
 from nenen88 import tempe
-import json, os
+import json
+import os
 
 SM = None
 
 try:
     from KANDANG import TEMPPATH, HOMEPATH
-    tmp = Path(TEMPPATH)
+    TMP = Path(TEMPPATH)
     HOME = Path(HOMEPATH)
     SM = False
 except ImportError:
-    tmp = Path('/tmp')
+    TMP = Path('/tmp')
     HOME = Path.home()
     SM = True
 
 marked = Path(__file__).parent / 'marking.json'
 
-def purge():
+SyS = get_ipython().system
+CD = os.chdir
+
+def purgeVAR():
     var_list = [
-        'WebUI', 'Models', 'WebUI_Output', 'Extensions', 'Embeddings',
+        'WebUI', 'Models', 'WebUI_Output', 'Extensions', 'Embeddings', 'UNET', 'CLIP',
         'VAE', 'CKPT', 'LORA', 'TMP_CKPT', 'TMP_LORA', 'Forge_SVD', 'Controlnet_Widget', 'Upscalers'
     ]
     for var in var_list:
         if var in globals():
             del globals()[var]
 
-def get_name(path):
-    with open(path, 'r') as file:
-        value = json.load(file)
-        return value.get('ui', None)
+def getWebUIName(path):
+    return json.load(open(path, 'r')).get('ui', None)
 
-def get_webui_paths():
-    ui = get_name(marked)
-    webui_paths = {
-        'A1111': 'A1111',
-        'Forge': 'Forge',
-        'ComfyUI': 'ComfyUI',
-        'ReForge': 'ReForge',
-        'FaceFusion': 'FaceFusion',
-        'SDTrainer': 'SDTrainer',
-        'SwarmUI': 'SwarmUI'
-    }
-
-    webui = HOME / webui_paths[ui] if ui in webui_paths else None
-
-    if ui in ['A1111', 'Forge', 'ReForge']:
-        webui_output = webui / 'outputs'
-    elif ui in ['ComfyUI', 'SDTrainer']:
-        webui_output = webui / 'output'
-    elif ui == 'SwarmUI':
-        webui_output = webui / 'Output'
-    else:
-        webui_output = None
-
-    return webui, webui_output
-
-def set_paths(ui):
-    webui_paths = {
+def setWebUIVAR(ui):
+    path_list = {
         'A1111': ('A1111', 'extensions', 'embeddings', 'VAE', 'Stable-diffusion', 'Lora', 'ESRGAN'),
         'Forge': ('Forge', 'extensions', 'embeddings', 'VAE', 'Stable-diffusion', 'Lora', 'ESRGAN'),
         'ComfyUI': ('ComfyUI', 'custom_nodes', 'embeddings', 'vae', 'checkpoints', 'loras', 'upscale_models'),
@@ -68,73 +45,54 @@ def set_paths(ui):
         'SwarmUI': ('SwarmUI', 'Extensions', 'Embeddings', 'VAE', 'Stable-Diffusion', 'Lora', 'upscale_models')
     }
 
-    if ui in webui_paths:
-        webui_name, ext, emb, v, c, l, u = webui_paths[ui]
-        webui = HOME / webui_name if webui_name else None
+    U, extension, embed, vae, ckpt, lora, upscaler = path_list[ui]
+    W = HOME / U if U else None
+    M = W / ('Models' if ui == 'SwarmUI' else 'models') if W else None
+    EXT = W / 'src' / extension if ui == 'SwarmUI' and extension else W / extension if extension else None
+    E = (
+        M / embed if ui in ['ComfyUI', 'SwarmUI'] else
+        W / embed if ui in ['A1111', 'Forge', 'ReForge'] else
+        None
+    )
+    V = M / vae if M and vae else None
+    C = M / ckpt if M and ckpt else None
+    L = M / lora if M and lora else None
+    UPS = M / upscaler if ui not in ['FaceFusion', 'SDTrainer'] else None
+    O = W / (
+        'Output' if ui == 'SwarmUI' else 'output' if ui in ['ComfyUI', 'SDTrainer'] else 'outputs'
+    ) if W else None
 
-        if ui == 'SDTrainer':
-            models = webui
-        elif ui == 'SwarmUI':
-            models = webui / 'Models'
-        else:
-            models = webui / 'models' if webui else None
-
-        if ui in ['A1111', 'Forge', 'ReForge']:
-            webui_output = webui / 'outputs'
-        elif ui in ['ComfyUI', 'SDTrainer']:
-            webui_output = webui / 'output'
-        elif ui == 'SwarmUI':
-            webui_output = webui / 'Output'
-        else:
-            webui_output = None
-
-        extensions = (
-            webui / 'src' / ext if ui == 'SwarmUI' and ext else
-            webui / ext if ext else
-            None
-        )
-
-        embeddings = (
-            models / emb if ui in ['ComfyUI', 'SwarmUI'] else
-            webui / emb if ui in ['A1111', 'Forge', 'ReForge'] else
-            None
-        )
-
-        vae = models / v if models and v else None
-        ckpt = models / c if models and c else None
-        lora = models / l if models and l else None
-
-        upscale = models / u if ui not in ['FaceFusion', 'SDTrainer'] else None
-
-        return webui, models, webui_output, extensions, embeddings, vae, ckpt, lora, upscale
+    return W, M, O, EXT, E, V, C, L, UPS
 
 if SM:
     @register_line_magic
     def clear_output_images(line):
-        ui = get_name(marked)
-        _, webui_output = get_webui_paths()
-        get_ipython().system(f"rm -rf {webui_output}/* {HOME / '.cache/*'}")
-        os.chdir(HOME)
+        ui = getWebUIName(marked)
+        _, _, output, _, _, _, _, _, _ = setWebUIVAR(ui)
+        SyS(f"rm -rf {output}/* {HOME / '.cache/*'}")
+        CD(HOME)
         print(f'{ui} outputs cleared.')
 
     @register_line_magic
     def uninstall_webui(line):
-        ui = get_name(marked)
-        webui, _ = get_webui_paths()
-        get_ipython().system(f"rm -rf {webui} {HOME / 'tmp'} {HOME / '.cache/*'}")
+        ui = getWebUIName(marked)
+        webui, _, _, _, _, _, _, _, _ = setWebUIVAR(ui)
+        SyS(f"rm -rf {webui} {HOME / 'tmp'} {HOME / '.cache/*'}")
         print(f'{ui} uninstalled.')
-        os.chdir(HOME)
+        CD(HOME)
         get_ipython().kernel.do_shutdown(True)
 
 if marked.exists():
-    purge()
+    purgeVAR()
 
-    ui = get_name(marked)
-    WebUI, Models, WebUI_Output, Extensions, Embeddings, VAE, CKPT, LORA, Upscalers = set_paths(ui)
+    ui = getWebUIName(marked)
+    WebUI, Models, WebUI_Output, Extensions, Embeddings, VAE, CKPT, LORA, Upscalers = setWebUIVAR(ui)
 
     Controlnet_Widget = WebUI / 'asd/controlnet.py' if WebUI else None
-    Forge_SVD = tmp / 'svd' if ui in ['Forge', 'ReForge'] else None
-    TMP_CKPT = tmp / 'ckpt'
-    TMP_LORA = tmp / 'lora'
+    Forge_SVD = TMP / 'svd' if ui in ['Forge', 'ReForge'] else None
+    UNET = TMP / 'unet' if ui in ['Forge', 'ComfyUI', 'SwarmUI'] else None
+    CLIP = TMP / 'clip' if ui in ['Forge', 'ComfyUI', 'SwarmUI'] else None
+    TMP_CKPT = TMP / 'ckpt'
+    TMP_LORA = TMP / 'lora'
 
     tempe()
