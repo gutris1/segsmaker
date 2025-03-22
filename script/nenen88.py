a661f0393984920c9be17aa4a1c76db9d3d90947
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-03-23 01:40:48 +0700

    Update nenen88.py

--- script/nenen88.py
+++ script/nenen88.py
@@ -32,11 +32,11 @@ def say(line):
 
         if re.match(r'^\{[^\{\}]+\}$', msg.lower()):
             color = msg[1:-1]
-            msg = ""
+            msg = ''
         else:
             while i < len(args) - 1 and not re.match(r'^\{[^\{\}]+\}$', args[i + 1].lower()):
                 i += 1
-                msg += " " + args[i]
+                msg += ' ' + args[i]
 
         if color == 'd':
             color = default_color
@@ -45,20 +45,20 @@ def say(line):
                 color = args[i + 1][1:-1]
                 i += 1
 
-        span_text = f"<span"
+        span_text = f'<span'
         if color:
             span_text += f" style='color:{color};'"
-        span_text += f">{msg}</span>"
+        span_text += f'>{msg}</span>'
         output.append(span_text)
         i += 1
 
-    display(HTML(" ".join(output)))
+    display(HTML(' '.join(output)))
 
 @register_line_magic
 def download(line):
     args = line.split()
     if not args:
-        print("  missing URL, downloading nothing")
+        print('  missing URL, downloading nothing')
         return
 
     url = args[0]
@@ -80,10 +80,10 @@ def netorare(line):
 
     path = lambda s: '/' in s or '~/' in s
 
-    aria2c = "aria2c --header='User-Agent: Mozilla/5.0' --allow-overwrite=true --console-log-level=error --stderr=true -c -x16 -s16 -k1M -j5"
+    aria2c = 'aria2c --header="User-Agent: Mozilla/5.0" --allow-overwrite=true --console-log-level=error --stderr=true -c -x16 -s16 -k1M -j5'
 
-    if TOBRUT and "huggingface.co" in url:
-        aria2c += f" --header='Authorization: Bearer {TOBRUT}'"
+    if TOBRUT and 'huggingface.co' in url:
+        aria2c += f' --header="Authorization: Bearer {TOBRUT}"'
 
     try:
         if len(parts) >= 3:
@@ -183,21 +183,21 @@ def ariari(cmd, fn):
             text=True
         )
 
-        aria2_output = ""
+        aria2_output = ''
         break_line = False
 
         error_code = []
         error_line = []
 
-        MAGENTA = "\033[35m"
-        RED = "\033[31m"
-        CYAN = "\033[36m"
-        GREEN = "\033[38;5;35m"
-        YELLOW = "\033[33m"
-        BLUE = "\033[38;5;69m"
-        PURPLE = "\033[38;5;135m"
-        ORANGE = "\033[38;5;208m"
-        RESET = "\033[0m"
+        MAGENTA = '\033[35m'
+        RED = '\033[31m'
+        CYAN = '\033[36m'
+        GREEN = '\033[38;5;35m'
+        YELLOW = '\033[33m'
+        BLUE = '\033[38;5;69m'
+        PURPLE = '\033[38;5;135m'
+        ORANGE = '\033[38;5;208m'
+        RESET = '\033[0m'
 
         while True:
             lines = p.stderr.readline()
@@ -213,7 +213,7 @@ def ariari(cmd, fn):
                         prog = re.sub(r'(\|\s*)(error_line)(\s*\|)', f'\\1{RED}\\2{RESET}\\3', prog)
                         first, _, last = prog.rpartition('|')
                         last = re.sub(r'/', f'{CYAN}/{RESET}', last)
-                        prog = f"{first}|{last}"
+                        prog = f'{first}|{last}'
                         error_line.append(prog)
 
                     if re.match(r'\[#\w{6}\s.*\]', prog):
@@ -228,31 +228,31 @@ def ariari(cmd, fn):
 
                         lines = prog.splitlines()
                         for line in lines:
-                            print(f"\r{' '*300}\r {line}", end="")
+                            print(f"\r{' '*300}\r {line}", end='')
                             sys.stdout.flush()
 
                         break_line = True
                         break
 
         error = error_code + error_line
-        for lines in error: print(f"  {lines}")
+        for lines in error: print(f'  {lines}')
 
         break_line and print()
 
-        stripe = aria2_output.find("======+====+===========")
+        stripe = aria2_output.find('======+====+===========')
         if stripe != -1:
             for lines in aria2_output[stripe:].splitlines():
                 if '|' in lines and 'OK' in lines:
                     lines = re.sub(r'(\|\s*)(OK)(\s*\|)', f'\\1{GREEN}\\2{RESET}\\3', lines)
                     first, _, last = lines.rpartition('|')
                     last = re.sub(r'/', f'{ORANGE}/{RESET}', last)
-                    lines = f"{first}|{last}"
-                    print(f"  {lines}")
+                    lines = f'{first}|{last}'
+                    print(f'  {lines}')
 
         p.wait()
 
     except KeyboardInterrupt:
-        print(f"\n{'':>2}^ Canceled")
+        print(f'\n{'':>2}^ Canceled')
 
 def curlly(cmd, fn):
     try:
@@ -263,12 +263,12 @@ def curlly(cmd, fn):
         )
 
         prog = re.compile(r'(\d+\.\d+)%')
-        curl_output = ""
+        curl_output = ''
 
         with tqdm(
-            total=100, desc=f"{fn.ljust(58):>{58 + 2}}", initial=0,
-            bar_format="{desc} 【{bar:20}】【{percentage:3.0f}%】",
-            ascii="▷▶", file=sys.stdout
+            total=100, desc=f'{fn.ljust(58):>{58 + 2}}', initial=0,
+            bar_format='{desc} 【{bar:20}】【{percentage:3.0f}%】',
+            ascii='▷▶', file=sys.stdout
         ) as pbar:
             for line in iter(p.stderr.readline, ''):
                 if line.strip():
@@ -284,13 +284,13 @@ def curlly(cmd, fn):
         p.wait()
 
         if p.returncode != 0:
-            if "curl: (23)" in curl_output:
+            if 'curl: (23)' in curl_output:
                 print(
                     f"{'':>2}^ File already exists; download skipped. "
                     "Append a custom name after the URL or PATH to overwrite."
                 )
-            elif "curl: (3)" in curl_output:
-                print("")
+            elif 'curl: (3)' in curl_output:
+                print('')
             else:
                 print(f"{'':>2}^ Error: {curl_output}")
         else:
@@ -304,14 +304,14 @@ def clone(i):
     p = Path(i).expanduser()
 
     def proc(line):
-        return line.strip()[len("git clone "):].strip() if line.strip().startswith("git clone") else line.strip()
+        return line.strip()[len('git clone '):].strip() if line.strip().startswith('git clone') else line.strip()
 
-    if p.suffix == ".txt" and p.is_file():
-        cmds = [f"git clone {proc(line)}" for line in p.read_text().splitlines()]
+    if p.suffix == '.txt' and p.is_file():
+        cmds = [f'git clone {proc(line)}' for line in p.read_text().splitlines()]
     elif isinstance(i, str):
-        cmds = [f"git clone {proc(i)}"]
+        cmds = [f'git clone {proc(i)}']
     else:
-        cmds = [f"git clone {proc(l)}" for l in i]
+        cmds = [f'git clone {proc(l)}' for l in i]
 
     for cmd in cmds:
         cmd = cmd.strip()
@@ -329,11 +329,11 @@ def clone(i):
                 break
 
             if output := output.strip():
-                if "fatal" in output:
-                    print(f"  {output}")
-                elif output.startswith("Cloning into"):
-                    repo_name = "/".join(output.split("'")[1].split("/")[-3:])
-                    print(f"  {repo_name} ▶ {url}")
+                if 'fatal' in output:
+                    print(f'  {output}')
+                elif output.startswith('Cloning into'):
+                    repo_name = '/'.join(output.split('"')[1].split('/')[-3:])
+                    print(f'  {repo_name} ▶ {url}')
 
         p.wait()
 
@@ -393,26 +393,19 @@ def tempe(line=''):
             ENVNAME = name
             break
 
-    if ENVNAME in ('Colab', 'Kaggle'):
-        tmplist = [
-            f"{BASEPATH}/temp/ckpt",
-            f"{BASEPATH}/temp/lora",
-            f"{BASEPATH}/temp/controlnet",
-            f"{BASEPATH}/temp/svd",
-            f"{BASEPATH}/temp/z123",
-            f"{BASEPATH}/temp/clip",
-            f"{BASEPATH}/temp/unet"
-        ]
+    DIRS = [
+        'ckpt',
+        'lora',
+        'controlnet',
+        'svd',
+        'z123',
+        'clip',
+        'clip_vision',
+        'diffusers',
+        'diffusion_models',
+        'text_encoders',
+        'unet'
+    ]
 
-    else:
-        tmplist = [
-            "/tmp/ckpt",
-            "/tmp/lora",
-            "/tmp/controlnet",
-            "/tmp/svd",
-            "/tmp/z123",
-            "/tmp/clip",
-            "/tmp/unet"
-        ]
-
-    for paths in tmplist: Path(paths).mkdir(parents=True, exist_ok=True)
+    TMP = f'{BASEPATH}/temp' if ENVNAME in ('Colab', 'Kaggle') else '/tmp'
+    for SUB in DIRS: Path(f'{TMP}/{SUB}').mkdir(parents=True, exist_ok=True)
