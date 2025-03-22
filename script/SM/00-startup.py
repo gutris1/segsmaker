ce1d011e606d91c1997e81172eeef18e08d27920
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-03-23 05:58:43 +0700

    Update 00-startup.py

--- script/SM/00-startup.py
+++ script/SM/00-startup.py
@@ -7,18 +7,17 @@ home = Path.home()
 marking = home / '.gutris1/marking.py'
 zrok_bin = home / '.zrok/bin/zrok'
 ngrok_bin = home / '.ngrok/bin/ngrok'
-startup = home / '.ipython/profile_default/startup'
+STR = home / '.ipython/profile_default/startup'
 
 iRON = os.environ
-
-sys.path.append(str(startup))
+sys.path.append(str(STR))
 
 if zrok_bin.exists() and str(zrok_bin.parent) not in iRON.get('PATH', ''):
     zrok_bin.chmod(0o755)
     iRON['PATH'] += ':' + str(zrok_bin.parent)
 
 if ngrok_bin.exists() and str(ngrok_bin.parent) not in iRON.get('PATH', ''):
-        ngrok_bin.chmod(0o755)
-        iRON['PATH'] += ':' + str(ngrok_bin.parent)
+    ngrok_bin.chmod(0o755)
+    iRON['PATH'] += ':' + str(ngrok_bin.parent)
 
 if marking.exists(): get_ipython().run_line_magic('run', str(marking))
