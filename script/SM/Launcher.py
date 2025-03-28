92eae7524ee42c297751b096ab19700fcec0af77
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-03-29 00:50:02 +0700

    Update Launcher.py

--- script/SM/Launcher.py
+++ script/SM/Launcher.py
@@ -15,11 +15,9 @@ cwd = Path.cwd()
 iRON = os.environ
 SyS = os.system
 
-TCM = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
-VENVLIB = '/tmp/venv/lib'
-VENVBIN = '/tmp/venv/bin'
-
 def setENV():
+    TCM = '/home/studio-lab-user/.conda/envs/default/lib/libtcmalloc_minimal.so.4'
+
     if ui in ['FaceFusion', 'SDTrainer']:
         import matplotlib
         iRON['MPLBACKEND'] = 'gtk3agg'
@@ -40,12 +38,19 @@ def setENV():
                 iRON['LD_PRELOAD'] = TCM
 
     else:
+        if ui == 'ComfyUI':
+            VENVLIB = '/tmp/venv-comfyui/lib'
+            VENVBIN = '/tmp/venv-comfyui/bin'
+        else:
+            VENVLIB = '/tmp/venv/lib'
+            VENVBIN = '/tmp/venv/bin'
+
         if 'LD_PRELOAD' not in iRON or TCM not in iRON.get('LD_PRELOAD', ''):
             iRON['LD_PRELOAD'] = TCM
         if 'LD_LIBRARY_PATH' not in iRON or VENVLIB not in iRON.get('LD_LIBRARY_PATH', ''):
-            iRON['LD_LIBRARY_PATH'] = VENVLIB + ":" + iRON.get('LD_LIBRARY_PATH', '')
-        if VENVBIN not in iRON.get("PATH", ''):
-            iRON["PATH"] = VENVBIN + ":" + iRON.get("PATH", '')
+            iRON['LD_LIBRARY_PATH'] = VENVLIB + ':' + iRON.get('LD_LIBRARY_PATH', '')
+        if VENVBIN not in iRON.get('PATH', ''):
+            iRON['PATH'] = VENVBIN + ':' + iRON.get('PATH', '')
 
         if ui == 'SwarmUI':
             iRON['SWARMPATH'] = str(cwd)
@@ -54,24 +59,22 @@ def setENV():
     iRON['PYTHONWARNINGS'] = 'ignore'
 
 def Launch():
-    launcher = 'main.py' if ui == 'ComfyUI' else 'launch.py'
-
     if ui == 'SwarmUI':
         SyS('pip install -q rembg')
         SyS('git pull -q')
         cmd = 'bash ./launch-linux.sh ' + ' '.join(sys.argv[1:])
 
     else:
+        launcher = 'main.py' if ui == 'ComfyUI' else 'launch.py'
+
         if ui in ['A1111', 'Forge', 'ReForge']:
-            timer = cwd / "asd/pinggytimer.txt"
+            timer = cwd / 'asd/pinggytimer.txt'
             end_time = int(time.time()) + 3600
-            SyS(f"echo -n {end_time} > {timer}")
+            SyS(f'echo -n {end_time} > {timer}')
 
             if ui == 'Forge':
-                FT = cwd / "FT.txt"
-                if not FT.exists():
-                    SyS('pip uninstall -qy transformers')
-                    FT.write_text("blyat")
+                FT = cwd / 'FT.txt'
+                FT.exists() or (SyS('pip uninstall -qy transformers'), FT.write_text('blyat'))
 
         cmd = f'python3 {launcher} ' + ' '.join(sys.argv[1:])
 
@@ -84,7 +87,7 @@ def sdtrainer_launch():
 def facefusion_launch():
     dmc = 'source activate default && /tmp/venv-fusion/bin/python3 facefusion.py run'
     cmd = f"{dmc} {' '.join(shlex.quote(arg) for arg in sys.argv[1:])}"
-    webui = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=sys.stdout, text=True, shell=True, executable="/bin/bash")
+    webui = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=sys.stdout, text=True, shell=True, executable='/bin/bash')
     for line in webui.stdout:
         print(line, end='')
     webui.wait()
