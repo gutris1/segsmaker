419a2eb2303d0758166763ced161e088acf9f772
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-09-17 20:53:50 +0700

    SMSL default ENV (#90)
    * ass
    
    * d
    
    * l

---
+++ script/SM/default.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import os
+
+conda = Path('/home/studio-lab-user/.conda/envs/default')
+py = conda / 'bin/python'
+SyS = os.system
+
+if not py.exists():
+    SyS(f'conda create -qy -p {conda} python=3.10.13')
+    SyS(f'{py} -m pip install -q ipykernel ipywidgets')
+    SyS(f'{py} -m ipykernel install --user --name default --display-name "default"')
+    print('\nRestart JupyterLab... then switch kernel to default:Python')
