4b23a91f1d823f9f667d23bf6084e70ca0c67a0e
gutris1 <132797949+gutris1@users.noreply.github.com>
2026-06-17 19:24:01 +0700

    Update venv.py

--- script/SM/venv.py
+++ script/SM/venv.py
@@ -16,28 +16,23 @@ IMG = SRC / 'loading.png'
 tmp = Path('/tmp')
 cwd = Path.cwd()
 
-vnv_FF = tmp / 'venv-fusion'
-vnv_SDT = tmp / 'venv-sd-trainer'
+vnv_D = tmp / 'venv'
 vnv_C = tmp / 'venv-comfy-swarm'
 vnv_FC = tmp / 'python311'
-vnv_D = tmp / 'venv'
 
 SyS = get_ipython().system
 CD = os.chdir
 
 def aDel():
     for name in ['tempe', 'say', 'download']:
-        if name in globals():
-            del globals()[name]
+        globals().pop(name, None)
 
 def trashing():
-    dirs1 = ['A1111', 'Forge', 'ReForge', 'Forge-Classic', 'ComfyUI', 'SwarmUI', 'FaceFusion', 'SDTrainer']
+    dirs1 = ['A1111', 'Forge', 'ReForge', 'Forge-Classic', 'ComfyUI', 'SwarmUI']
     dirs2 = ['ckpt', 'lora', 'controlnet', 'svd', 'z123']
 
-    paths = [HOME / name for name in dirs1] + [tmp / name for name in dirs2]
-    for path in paths:
-        cmd = f'find {path} -type d -name .ipynb_checkpoints -exec rm -rf {{}} +'
-        SyS(f'{cmd} > /dev/null 2>&1')
+    for path in [HOME / d for d in dirs1] + [tmp / d for d in dirs2]:
+        SyS(f'find {path} -type d -name .ipynb_checkpoints -exec rm -rf {{}} + > /dev/null 2>&1')
 
 def check_pv():
     try:
@@ -47,86 +42,57 @@ def check_pv():
         cmd = 'mamba install -y pv'
         subprocess.run(shlex.split(cmd), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-def check_venv(folder):
-    return int(du[0].split()[0]) if (du := get_ipython().getoutput(f'du -s -b {folder}')) else 0
-
-def check_tmp(path):
-    return (stats := os.statvfs(path)).f_frsize * stats.f_bavail
-
-def listing(directory):
-    return [(path := Path(root) / file, path.stat().st_size) for root, _, files in os.walk(directory) for file in files]
-
-def removing(directory, req_space):
-    files = listing(directory)
-    files.sort(key=lambda x: x[1], reverse=True)
-    freed_space = 0
-
-    for file_path, size in files:
-        if freed_space >= req_space:
-            break
-
-        print(f'Removing {file_path}')
-        SyS(f'rm -rf {file_path}')
-        freed_space += size
-    return freed_space
-
 def unused_venv(vnv):
-    vl = [vnv_FF, vnv_SDT, vnv_D, vnv_C, vnv_FC]
-    vd = [v for v in vl if v != vnv and v.exists()]
-    if vd:
-        rmf = f"rm -rf {' '.join(f'{v}/* {v}' for v in vd)}"
-        SyS(rmf)
+    venvs = [vnv_D, vnv_C, vnv_FC]
+    unused = [v for v in venvs if v != vnv and v.exists()]
+
+    if unused:
+        SyS(f"rm -rf {' '.join(f'{v}/* {v}' for v in unused)}")
 
 def load_config():
     config = json.load(MARK.open('r')) if MARK.exists() else {}
     ui = config.get('ui')
 
-    if ui == 'FaceFusion':
-        url = 'https://huggingface.co/gutris1/webui/resolve/main/env/venv-fusion.tar.lz4'
-        need_space = 13 * 1024**3
-        vnv = vnv_FF
-    elif ui == 'SDTrainer':
-        url = 'https://huggingface.co/gutris1/webui/resolve/main/env/venv-sd-trainer.tar.lz4'
-        need_space = 14 * 1024**3
-        vnv = vnv_SDT
-    elif ui in ['ComfyUI', 'SwarmUI']:
+    if ui in ['ComfyUI', 'SwarmUI']:
         url = 'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-ComfyUI-SwarmUI-Torch260-cu124.tar.lz4'
-        need_space = 14 * 1024**3
         vnv = vnv_C
+
     elif ui == 'Forge-Classic':
         url = 'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-FC-Python311-Torch260-cu124.tar.lz4'
-        need_space = 14 * 1024**3
         vnv = vnv_FC
+
     else:
-        url = 'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-Torch260-cu124.tar.lz4'
-        need_space = 14 * 1024**3
+        url = [
+            'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-Torch2120-cu130-part1.tar.lz4',
+            'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-Torch2120-cu130-part2.tar.lz4'
+        ]
         vnv = vnv_D
 
-    fn = Path(url).name
-    return ui, url, need_space, vnv, fn
+    fn = [Path(u).name for u in url] if isinstance(url, list) else Path(url).name
+    return ui, url, vnv, fn
 
-def install_venv(ui, url, need_space, vnv, fn):
+def install_venv(ui, url, vnv, fn):
     unused_venv(vnv)
     check_pv()
 
     clear_output(wait=True)
     display(Image(filename=str(IMG)))
 
-    free_space = check_tmp(tmp)
-    req_space = need_space - free_space
+    CD(HOME)
 
-    if req_space > 0:
-        print(f'Need space {req_space / 1024**3:.1f} GB for VENV')
-        for path in [tmp / 'ckpt', tmp / 'lora', tmp / 'controlnet', tmp / 'clip', tmp / 'unet']:
-            if req_space > 0: req_space -= removing(path, req_space)
-
-    CD(tmp)
     msg = 'Installing Forge Classic Python 3.11.13' if ui == 'Forge-Classic' else f'Installing {ui} VENV'
     say(f'<b>【{{red}} {msg}{{d}} 】{{red}}</b>')
-    download(url)
 
-    SyS(f'pv {fn} | lz4 -d | tar xf -')
-    Path(fn).unlink()
+    if isinstance(url, list):
+        for u, f in zip(url, fn):
+            download(u)
+            SyS(f'cd {tmp} && pv "{HOME / f}" | lz4 -d | tar xf -')
+            Path(HOME / f).unlink(missing_ok=True)
+
+    else:
+        download(url)
+        SyS(f'cd {tmp} && pv "{HOME / fn}" | lz4 -d | tar xf -')
+        Path(HOME / fn).unlink(missing_ok=True)
 
     if ui != 'Forge-Classic':
         for cmd in [
@@ -135,15 +101,17 @@ def install_venv(ui, url, need_space, vnv, fn):
             f'{pip} install -U --force-reinstall pip',
             f'{pip} install ipykernel matplotlib pyyaml',
             f'{pip} install -q comfy-aimdo'
-        ]: SyS(f'{cmd} > /dev/null 2>&1')
+        ]:
+            SyS(f'{cmd} > /dev/null 2>&1')
 
 print('checking env...')
 tempe()
 
-ui, url, need_space, vnv, fn = load_config()
+ui, url, vnv, fn = load_config()
 pip = f'{vnv}/bin/python3 -m pip'
 
-if not vnv.exists(): install_venv(ui, url, need_space, vnv, fn)
+if not vnv.exists():
+    install_venv(ui, url, vnv, fn)
 
 clear_output(wait=True)
 trashing()
