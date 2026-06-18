b30ef56882e04ee1f62e80bb59073f8e87f8ea9c
gutris1 <132797949+gutris1@users.noreply.github.com>
2026-06-18 16:59:12 +0700

    Update 00-startup.py

--- script/SM/00-startup.py
+++ script/SM/00-startup.py
@@ -3,21 +3,21 @@ from pathlib import Path
 import sys
 import os
 
-home = Path.home()
-marking = home / '.gutris1/marking.py'
-zrok_bin = home / '.zrok/bin/zrok'
-ngrok_bin = home / '.ngrok/bin/ngrok'
-STR = home / '.ipython/profile_default/startup'
-
 iRON = os.environ
-sys.path.append(str(STR))
 
-if zrok_bin.exists() and str(zrok_bin.parent) not in iRON.get('PATH', ''):
-    zrok_bin.chmod(0o755)
-    iRON['PATH'] += ':' + str(zrok_bin.parent)
+home = Path.home()
+m = home / '.gutris1/marking.py'
+zrok = home / '.zrok2/zrok2'
+ngrok = home / '.ngrok/ngrok'
+startup = home / '.ipython/profile_default/startup'
+sys.path.append(str(startup))
 
-if ngrok_bin.exists() and str(ngrok_bin.parent) not in iRON.get('PATH', ''):
-    ngrok_bin.chmod(0o755)
-    iRON['PATH'] += ':' + str(ngrok_bin.parent)
+if zrok.exists() and str(zrok.parent) not in iRON.get('PATH', ''):
+    zrok.chmod(0o755)
+    iRON['PATH'] += ':' + str(zrok.parent)
 
-if marking.exists(): get_ipython().run_line_magic('run', str(marking))
+if ngrok.exists() and str(ngrok.parent) not in iRON.get('PATH', ''):
+        ngrok.chmod(0o755)
+        iRON['PATH'] += ':' + str(ngrok.parent)
+
+if m.exists(): get_ipython().run_line_magic('run', str(m))
