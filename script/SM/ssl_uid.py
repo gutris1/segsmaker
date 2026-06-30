ce9ac945c7fa72456e67ece22df87e1cbc17ee5d
gutris1 <132797949+gutris1@users.noreply.github.com>
2026-07-01 03:22:21 +0700

    Create ssl_uid.py

---
+++ script/SM/ssl_uid.py
@@ -0,0 +1,223 @@
+from pathlib import Path
+
+HOME = Path.home()
+TMP = Path('/tmp')
+
+URL = {
+    'D': [
+        'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-Torch2120-cu130-part1.tar.lz4',
+        'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-Torch2120-cu130-part2.tar.lz4'
+    ],
+    'FC': 'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-FC-Python311-Torch260-cu124.tar.lz4',
+    'FN': [
+        'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-FN-Torch2121-cu130-part1.tar.lz4',
+        'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-FN-Torch2121-cu130-part2.tar.lz4'
+    ],
+    'CS': 'https://huggingface.co/gutris1/webui/resolve/main/env/SSL-ComfyUI-SwarmUI-Torch260-cu124.tar.lz4',
+}
+
+UID = {
+    'A1111': {
+        'r': 'https://github.com/gutris1/A1111',
+        'b': 'master',
+        'sym': lambda M: [
+            f"rm -rf {M / 'Stable-diffusion/tmp_ckpt'} {M / 'Lora/tmp_lora'} {M / 'ControlNet'}"
+        ],
+        'links': lambda M: [
+            (TMP, HOME / 'tmp'),
+            (TMP / 'ckpt', M / 'Stable-diffusion/tmp_ckpt'),
+            (TMP / 'lora', M / 'Lora/tmp_lora'),
+            (TMP / 'controlnet', M / 'ControlNet')
+        ],
+
+        'python': '/tmp/venv',
+        'url': URL['D'],
+
+        'title': 'A1111',
+        'args': '--xformers',
+        'py': '/tmp/venv/bin/python3',
+        'lib': '/tmp/venv/lib',
+        'bin': '/tmp/venv/bin',
+        'cpu': '--use-cpu all --precision full --no-half --skip-torch-cuda-test',
+    },
+
+    'Forge': {
+        'r': 'https://github.com/lllyasviel/stable-diffusion-webui-forge Forge',
+        'b': 'main',
+        'sym': lambda M: [
+            f"rm -rf {M / 'Stable-diffusion/tmp_ckpt'} {M / 'Lora/tmp_lora'} {M / 'ControlNet'}",
+            f"rm -rf {M / 'svd'} {M / 'z123'} {M / 'clip'} {M / 'clip_vision'} {M / 'diffusers'}",
+            f"rm -rf {M / 'diffusion_models'} {M / 'text_encoder'} {M / 'unet'}"
+        ],
+        'links': lambda M: [
+            (TMP, HOME / 'tmp'),
+            (TMP / 'ckpt', M / 'Stable-diffusion/tmp_ckpt'),
+            (TMP / 'lora', M / 'Lora/tmp_lora'),
+            (TMP / 'controlnet', M / 'ControlNet'),
+            (TMP / 'z123', M / 'z123'),
+            (TMP / 'svd', M / 'svd'),
+            (TMP / 'clip', M / 'clip'),
+            (TMP / 'clip_vision', M / 'clip_vision'),
+            (TMP / 'diffusers', M / 'diffusers'),
+            (TMP / 'diffusion_models', M / 'diffusion_models'),
+            (TMP / 'text_encoders', M / 'text_encoder'),
+            (TMP / 'unet', M / 'unet')
+        ],
+
+        'python': '/tmp/venv',
+        'url': URL['D'],
+
+        'title': 'Forge',
+        'args': '--disable-xformers --opt-sdp-attention --cuda-stream',
+        'py': '/tmp/venv/bin/python3',
+        'lib': '/tmp/venv/lib',
+        'bin': '/tmp/venv/bin',
+        'cpu': '--always-cpu --skip-torch-cuda-test',
+    },
+
+    'ReForge': {
+        'r': 'https://github.com/Panchovix/stable-diffusion-webui-reForge ReForge',
+        'b': 'main',
+        'sym': lambda M: [
+            f"rm -rf {M / 'Stable-diffusion/tmp_ckpt'} {M / 'Lora/tmp_lora'} {M / 'ControlNet'}",
+            f"rm -rf {M / 'svd'} {M / 'z123'}"
+        ],
+        'links': lambda M: [
+            (TMP, HOME / 'tmp'),
+            (TMP / 'ckpt', M / 'Stable-diffusion/tmp_ckpt'),
+            (TMP / 'lora', M / 'Lora/tmp_lora'),
+            (TMP / 'controlnet', M / 'ControlNet'),
+            (TMP / 'z123', M / 'z123'),
+            (TMP / 'svd', M / 'svd')
+        ],
+
+        'python': '/tmp/venv',
+        'url': URL['D'],
+
+        'title': 'ReForge',
+        'args': '--xformers --cuda-stream',
+        'py': '/tmp/venv/bin/python3',
+        'lib': '/tmp/venv/lib',
+        'bin': '/tmp/venv/bin',
+        'cpu': '--always-cpu --skip-torch-cuda-test',
+    },
+
+    'ReForge-old': {
+        'r': '-b main-old https://github.com/Panchovix/stable-diffusion-webui-reForge ReForge-old',
+        'b': 'main-old',
+
+        'python': '/tmp/venv',
+        'url': URL['D'],
+
+        'title': 'ReForge old',
+        'args': '--xformers --cuda-stream',
+        'py': '/tmp/venv/bin/python3',
+        'lib': '/tmp/venv/lib',
+        'bin': '/tmp/venv/bin',
+        'cpu': '--always-cpu --skip-torch-cuda-test',
+    },
+
+    'Forge-Classic': {
+        'r': '-b classic https://github.com/Haoming02/sd-webui-forge-classic Forge-Classic',
+        'b': 'classic',
+        'sym': lambda M: [
+            f"rm -rf {M / 'Stable-diffusion/tmp_ckpt'} {M / 'Lora/tmp_lora'} {M / 'ControlNet'}"
+        ],
+        'links': lambda M: [
+            (TMP, HOME / 'tmp'),
+            (TMP / 'ckpt', M / 'Stable-diffusion/tmp_ckpt'),
+            (TMP / 'lora', M / 'Lora/tmp_lora'),
+            (TMP / 'controlnet', M / 'ControlNet')
+        ],
+
+        'python': '/tmp/python311',
+        'url': URL['FC'],
+
+        'title': 'Forge Classic',
+        'args': '--xformers --cuda-stream --persistent-patches',
+        'py': '/tmp/python311/bin/python3',
+        'lib': '/tmp/python311/lib',
+        'bin': '/tmp/python311/bin',
+        'cpu': '--always-cpu --skip-torch-cuda-test',
+        'ld': True,
+    },
+
+    'Forge-Neo': {
+        'r': '-b neo https://github.com/Haoming02/sd-webui-forge-classic Forge-Neo',
+        'b': 'neo',
+
+        'python': '/tmp/NEO',
+        'url': URL['FN'],
+
+        'title': 'Forge Neo',
+        'args': '--xformers --cuda-stream',
+        'py': '/tmp/NEO/bin/python3',
+        'lib': '/tmp/NEO/lib',
+        'bin': '/tmp/NEO/bin',
+        'cpu': '--cpu --skip-torch-cuda-test',
+        'ld': True,
+        'cm': True,
+    },
+
+    'ComfyUI': {
+        'r': 'https://github.com/comfyanonymous/ComfyUI',
+        'b': 'master',
+        'sym': lambda M: [
+            f"rm -rf {M / 'checkpoints/tmp_ckpt'} {M / 'loras/tmp_lora'} {M / 'controlnet'}",
+            f"rm -rf {M / 'clip'} {M / 'clip_vision'} {M / 'diffusers'} {M / 'diffusion_models'}",
+            f"rm -rf {M / 'text_encoders'} {M / 'unet'}"
+        ],
+        'links': lambda M: [
+            (M / 'checkpoints', M / 'checkpoints_symlink'),
+            (TMP, HOME / 'tmp'),
+            (TMP / 'ckpt', M / 'checkpoints/tmp_ckpt'),
+            (TMP / 'lora', M / 'loras/tmp_lora'),
+            (TMP / 'controlnet', M / 'controlnet'),
+            (TMP / 'clip', M / 'clip'),
+            (TMP / 'clip_vision', M / 'clip_vision'),
+            (TMP / 'diffusers', M / 'diffusers'),
+            (TMP / 'diffusion_models', M / 'diffusion_models'),
+            (TMP / 'text_encoders', M / 'text_encoders'),
+            (TMP / 'unet', M / 'unet')
+        ],
+
+        'python': '/tmp/venv-comfy-swarm',
+        'url': URL['CS'],
+
+        'title': 'ComfyUI',
+        'args': '--dont-print-server --use-pytorch-cross-attention',
+        'py': '/tmp/venv-comfy-swarm/bin/python3',
+        'lib': '/tmp/venv-comfy-swarm/lib',
+        'bin': '/tmp/venv-comfy-swarm/bin',
+        'port': 8188,
+        'cpu': '--cpu',
+    },
+
+    'SwarmUI': {
+        'r': 'https://github.com/mcmonkeyprojects/SwarmUI',
+        'b': 'master',
+       'sym': lambda M: [
+            f"rm -rf {M / 'Stable-Diffusion/tmp_ckpt'} {M / 'Lora/tmp_lora'} {M / 'controlnet'}",
+            f"rm -rf {M / 'clip'} {M / 'unet'}"
+        ],
+        'links': lambda M: [
+            (TMP, HOME / 'tmp'),
+            (TMP / 'ckpt', M / 'Stable-Diffusion/tmp_ckpt'),
+            (TMP / 'lora', M / 'Lora/tmp_lora'),
+            (TMP / 'controlnet', M / 'controlnet'),
+            (TMP / 'clip', M / 'clip'),
+            (TMP / 'unet', M / 'unet')
+        ],
+
+        'python': '/tmp/venv-comfy-swarm',
+        'url': URL['CS'],
+
+        'title': 'SwarmUI',
+        'args': '--launch_mode none',
+        'py': '/tmp/venv-comfy-swarm/bin/python3',
+        'lib': '/tmp/venv-comfy-swarm/lib',
+        'bin': '/tmp/venv-comfy-swarm/bin',
+        'port': 7801,
+        'env': {'SWARMPATH': lambda: str(Path.cwd()), 'SWARM_NO_VENV': 'true'},
+    },
+}
