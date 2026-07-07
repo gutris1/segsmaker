a24641b8a5ff0bd58c4ccea89bb46bb430a09064
gutris1 <132797949+gutris1@users.noreply.github.com>
2026-07-07 19:36:46 +0700

    Update venv.py

--- script/SM/venv.py
+++ script/SM/venv.py
@@ -7,65 +7,60 @@ import json
 import os
 
 from nenen88 import tempe, say, download
-from ssl_uid import UID
+from _segsmaker_ import HOME, SRC, UID
 
-SyS = get_ipython().system
 CD = os.chdir
+SyS = get_ipython().system
 
-HOME = Path.home()
-SRC = HOME / '.gutris1'
+def _del():
+    for n in ['tempe', 'say', 'download']:
+        globals().pop(n, None)
+
+def _trash():
+    f = ['ckpt', 'lora', 'controlnet', 'svd', 'z123']
+    for p in [HOME / ui] + [TMP / d for d in f]:
+        SyS(f'find {p} -type d -name .ipynb_checkpoints -exec rm -rf {{}} + > /dev/null 2>&1')
+
+def _check():
+    try: subprocess.run(['pv', '-V'], capture_output=True, text=True, check=True)
+    except Exception: subprocess.run(['mamba', 'install', '-y', 'pv'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def _unused(env):
+    v = [p for d in UID.values() if (p := d['py']['p']) != env and p.exists()]
+    if v: SyS(f"rm -rf {' '.join(map(str, v))}")
+
+def _install():
+    py = UID[ui]['py']
+    env, version, url = py['p'], py['v'], py['url']
+
+    if env.exists(): return
+
+    _unused(env)
+    _check()
+
+    clear_output(wait=True)
+    say(f"<b>【{{red}} {ui.replace('-', ' ')} Python {version}{{d}} 】{{red}}</b>")
+
+    CD(TMP)
+
+    for u in url if isinstance(url, list) else [url]:
+        f = TMP / Path(u).name
+        download(u)
+        SyS(f"pv '{f}' | lz4 -d | tar xf -")
+        f.unlink(missing_ok=True)
+
 MARK = SRC / 'marking.json'
 TMP = Path('/tmp')
 cwd = Path.cwd()
 
 ui = json.load(MARK.open()).get('ui')
 
-def aDel():
-    for name in ['tempe', 'say', 'download']:
-        globals().pop(name, None)
-
-def trashing():
-    f = ['ckpt', 'lora', 'controlnet', 'svd', 'z123']
-    for p in [HOME / ui] + [TMP / d for d in f]:
-        SyS(f'find {p} -type d -name .ipynb_checkpoints -exec rm -rf {{}} + > /dev/null 2>&1')
-
-def check_pv():
-    try:
-        subprocess.run(['pv', '-V'], capture_output=True, text=True, check=True)
-    except Exception:
-        subprocess.run(['mamba', 'install', '-y', 'pv'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-def unused_python(env):
-    unused = [d['env'] for d in UID.values() if d['env'] != env and d['env'].exists()]
-    if unused: SyS(f"rm -rf {' '.join(map(str, unused))}")
-
-def install_python():
-    env = UID[ui]['env']
-    if env.exists(): return
-
-    unused_python(env)
-    check_pv()
-
-    CD(TMP)
-
-    clear_output(wait=True)
-    say(f"<b>【{{red}} Installing {ui.replace('-', ' ')} Python{{d}} 】{{red}}</b>")
-
-    url = UID[ui]['url']
-    l = zip(url, [Path(u).name for u in url]) if isinstance(url, list) else [(url, Path(url).name)]
-
-    for u, f in l:
-        t = TMP / f
-        download(u)
-        SyS(f'pv "{t}" | lz4 -d | tar xf -')
-        t.unlink(missing_ok=True)
-
 print('checking python...')
 tempe()
-trashing()
+_trash()
 
-install_python()
-
+_install()
 clear_output(wait=True)
-aDel()
+
+_del()
 CD(cwd)
